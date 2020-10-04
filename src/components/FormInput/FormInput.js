570e2f06e16bca4a45c7c888c7d94200c570a23d
Nemanja K <nemanja.k@xola.com>
2020-10-04 11:13:35 +0200

    XOL-5620 Fix phone input children issue

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -2,17 +2,17 @@ import classNames from "classnames";
 import React, { cloneElement } from "react";
 import { FormGroup, Input, Label } from "reactstrap";
 
-const FormInput = ({ label, error, ...rest }) => {
+const FormInput = ({ label, error, children, ...rest }) => {
     return (
         <FormGroup>
             <Label htmlFor={rest.id} className={classNames({ "text-danger": error })}>
                 <span className={classNames({ "font-weight-bold": error })}>{label}</span> {error}
             </Label>
 
-            {rest.type === "select" || !rest.children ? (
+            {rest.type === "select" || !children ? (
                 <Input id={rest.id} invalid={!!error} {...rest} />
             ) : (
-                cloneElement(rest.children, rest)
+                cloneElement(children, rest)
             )}
         </FormGroup>
     );
