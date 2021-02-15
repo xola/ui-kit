99b95aab0947e0d66f0f15aa88557faa797cf1f0
Manoj Vaibhav <manoj.r@xola.com>
2021-02-15 15:18:35 +0530

    XOL-5885 Changed Webpack config to support arrow functions in class based components

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,16 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: { loader: "babel-loader", options: { presets: ["@babel/preset-react", "@babel/preset-env"] } },
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-react",
+                            "@babel/preset-env",
+                            { plugins: ["@babel/plugin-proposal-class-properties"] },
+                        ],
+                    },
+                },
             },
 
             {
