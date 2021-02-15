2d4d116c9447bd6d4f114911596d2764e9596d6d
Manoj Vaibhav <manoj.r@xola.com>
2021-02-15 13:44:35 +0530

    XOL-5885 Added Components in index file

--- src/index.js
+++ src/index.js
@@ -6,5 +6,10 @@ export { CountrySelect } from "./components/CountrySelect";
 export { PhoneInput } from "./components/PhoneInput";
 export { MiniStepper } from "./components/MiniStepper";
 export { ProductsSelector } from "./components/ProductsSelector";
+export { ScheduleEditor } from "./components/ScheduleEditor";
+export { DatePicker } from "./components/DatePicker";
+export { TimePicker } from "./components/TimePicker";
+export { Sidebar } from "./components/Sidebar";
+export { Modal } from "./components/Modal";
 
 export * from "reactstrap";
