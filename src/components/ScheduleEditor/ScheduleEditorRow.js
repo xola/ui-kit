889f5eeb0c19f28a4545bfe5735be7b3efe38332
Andrej Sovanek <andrej@xola.com>
2021-02-18 14:58:37 +0100

    XOL-5885 cleanup

--- src/components/ScheduleEditor/ScheduleEditorRow.js
+++ src/components/ScheduleEditor/ScheduleEditorRow.js
@@ -4,12 +4,12 @@ import { Col, FormGroup, Label } from "reactstrap";
 const ScheduleEditorRow = (props) => {
     return (
         <FormGroup row>
-            <Col sm={2}>
+            <Col sm={4}>
                 <Label className="float-right" for={props.for}>
                     {props.label}
                 </Label>
             </Col>
-            <Col sm={6}>{props.children}</Col>
+            <Col sm={8}>{props.children}</Col>
         </FormGroup>
     );
 };
