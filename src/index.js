778044d2cbb59c3b72566fc5783a7c749f77e4e6
Manoj Vaibhav <manoj.r@xola.com>
2021-02-24 20:30:06 +0530

    XOL-5885 Removed Modal Component

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ export { FormInput } from "./components/FormInput";
 export { CountrySelect } from "./components/CountrySelect";
 export { PhoneInput } from "./components/PhoneInput";
 export { MiniStepper } from "./components/MiniStepper";
-export { ProductsSelector } from "./components/ProductsSelector";
+export { ProductSelector } from "./components/ProductSelector";
 export { ScheduleEditor } from "./components/ScheduleEditor";
 export { DatePicker } from "./components/DatePicker";
 export { TimePicker } from "./components/TimePicker";
-export { Modal } from "./components/Modal";
 
 export * from "reactstrap";
