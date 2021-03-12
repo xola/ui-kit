a1f74ab86d1b4d41f89d75263861534f54dd8334
Nemanja K <nemanja.k@xola.com>
2021-03-12 15:24:23 +0100

    XOL-5885 Add Xola SVG icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles/index.scss";
 
+// Components
 export { Stepper, Step } from "./components/Stepper";
 export { FormInput } from "./components/FormInput";
 export { CountrySelect } from "./components/CountrySelect";
@@ -9,5 +10,9 @@ export { ProductSelector } from "./components/ProductSelector";
 export { ScheduleEditor } from "./components/ScheduleEditor";
 export { DatePicker, formatDate } from "./components/DatePicker";
 export { TimePicker } from "./components/TimePicker";
+
+// Icons
+export { CheckIcon } from "./icons/CheckIcon";
+export { TrashIcon } from "./icons/TrashIcon";
 
 export * from "reactstrap";
