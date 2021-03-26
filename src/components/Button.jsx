34b286320172f8dbc20c2e0b37291dbb9468bef7
Nemanja K <nemanja.k@xola.com>
2021-03-26 18:14:16 +0100

    XOL-5923 Set up Tailwind

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Button = ({ backgroundColor, ...rest }) => {
-    return <button style={{ backgroundColor }} {...rest} />;
+    return <button style={{ backgroundColor }} {...rest} className="bg-blue-500 text-white px-4 py-2 rounded" />;
 };
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    purge: [
+        "./src/**/*.js",
+        "./src/**/*.jsx",
+        "./node_modules/xola-ui-kit/src/**/*.js",
+        "./node_modules/xola-ui-kit/src/**/*.jsx",
+    ],
+
+    darkMode: false,
+
+    theme: {
+        extend: {},
+    },
+
+    variants: {
+        extend: {},
+    },
+
+    plugins: [],
+};
