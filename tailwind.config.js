4a8d15d97c63a58797cbfc2dfbeff55f8e3a40e9
Nemanja K <nemanja.k@xola.com>
2021-04-05 21:32:38 +0200

    XOL-5923 Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,14 +56,15 @@ const colors = {
 };
 
 module.exports = {
+    mode: "jit",
+    darkMode: false,
+
     purge: [
         "./src/**/*.js",
         "./src/**/*.jsx",
         "./node_modules/xola-ui-kit/src/**/*.js",
         "./node_modules/xola-ui-kit/src/**/*.jsx",
     ],
-
-    darkMode: false,
 
     theme: {
         colors: {
