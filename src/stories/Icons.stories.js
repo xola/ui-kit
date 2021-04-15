79288f91b926928cdc372646b83a7adb3443fc37
Rushi Vishavadia <rushi.v@gmail.com>
2021-04-15 15:50:43 +0530

    Forgot to add popover files + Icons

---
+++ src/stories/Icons.stories.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { ChevronRightIcon, StarIcon, UserIcon } from "..";
+
+export default {
+    title: "Icons",
+};
+
+export const Default = () => {
+    const icons = [<ChevronRightIcon />, <StarIcon />, <UserIcon />];
+    return <div className="flex flex-row flex-wrap gap-8">{icons.map((icon, idx) => getIcon(icon, idx))}</div>;
+};
+
+const getIcon = (icon, idx) => {
+    return (
+        <div key={idx}>
+            {icon}
+            <div class="pt-3 text-gray-dark">{icon.type.displayName}</div>
+        </div>
+    );
+};
