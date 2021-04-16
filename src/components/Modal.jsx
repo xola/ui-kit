cf52a177bbd7e8a0801af9994ebc59c7a4834520
Rushi Vishavadia <rushi.v@gmail.com>
2021-04-16 17:08:23 +0530

    XOL-5923 Ability to show/hide "x" button and close modal on click outside

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,21 +2,38 @@ import clsx from "clsx";
 import React, { Fragment, useState, useEffect } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 
-export const Modal = ({ size, show, children }) => {
-    const [open, setOpen] = useState(show);
-    const [Header, Body, Footer] = children; // Needs to be an array
+export const Modal = ({ size, show, showClose, closeOnClickOutside, onHide, children }) => {
+    const childCount = React.Children.count(children);
+    if (childCount < 3) {
+        console.warn(`You have an insufficient number of children ${childCount}, the modal may not behave as expected`)
+    }
 
-    size = size || 'md';
-    const width = `sm:max-w-${size}`;
-    const modalArgs = { width, Header, Body, Footer };
+    const [open, setOpen] = useState(show);
+    const [Header, Body, Footer] = children;
+
+    size = size || "md";
+    const width = `sm:max-w-${size}`; // Tailwind JIT won't pick this up
+    const modalArgs = { width, showClose, Header, Body, Footer };
+
+    // Decide what method to call when the user click's outside the modal
+    const onClickOutside = closeOnClickOutside ? setOpen : () => {};
 
     useEffect(() => {
+        // Invoked when someone outside this component wants the modal to open or close
         setOpen(show);
-    });
+    }, [show]);
+
+    useEffect(() => {
+        // Invoked when the modal asked itself to close (clicking "X" or outside the modal). Tell the parent the modal
+        // has closed
+        if (open !== show) {
+            onHide();
+        }
+    }, [open]);
 
     return (
         <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" static className="fixed z-10 inset-0 overflow-y-auto" open={open} onClose={setOpen}>
+            <Dialog as="div" static className="fixed z-10 inset-0 overflow-y-auto" open={open} onClose={onClickOutside}>
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <Transition.Child
                         as={Fragment}
@@ -58,8 +75,21 @@ Modal.Core = React.forwardRef((props, ref) => {
     return (
         <div ref={ref} className={modalClasses}>
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                    {props.showClose && (
+                        <div
+                            className="cursor-pointer text-xl text-gray hover:text-black"
+                            onClick={props.onClick}
+                        >
+                            ×
+                        </div>
+                    )}
+                </div>
                 <div className="sm:flex sm:items-start">
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 w-full">{props.Header}{props.Body}</div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 w-full">
+                        {props.Header}
+                        {props.Body}
+                    </div>
                 </div>
             </div>
 
--- src/stories/Modal.stories.js
+++ src/stories/Modal.stories.js
@@ -15,6 +15,18 @@ export default {
                 defaultValue: { summary: "xl" },
             },
         },
+        showClose: {
+            type: { required: false },
+            defaultValue: true,
+            description: "If the 'x' button should be shown",
+            control: { type: "boolean" },
+        },
+        closeOnClickOutside: {
+            type: { required: false },
+            defaultValue: true,
+            description: "Close the modal if user clicks outside it",
+            control: { type: "boolean" },
+        },
     },
 };
 
@@ -25,7 +37,7 @@ export const Default = (config) => {
     return (
         <div>
             <Button onClick={toggle}>Click me to launch a modal</Button>
-            <Modal show={show} toggle={toggle} size={config.size}>
+            <Modal {...config} show={show} toggle={toggle} onHide={toggle}>
                 <Modal.Header>My Modal Header</Modal.Header>
                 <Modal.Body>My Modal Body</Modal.Body>
                 <Modal.Footer>
