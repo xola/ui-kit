a1a68d74a9b0f41ebc8444e4fb67844c21db3f6f
Rushi Vishavadia <rushi@xola.com>
2021-04-19 08:55:03 +0530

    XOL-5923 JIT/PurgeCSS fix for dynamic spacing

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import clsx from "clsx";
 
+// This is required otherwise JIT won't pickup the dynamically generated classes like "space-x-2"
+const sizes = {
+    1: "space-x-1",
+    1.5: "space-x-1.5",
+    2: "space-x-2",
+    3: "space-x-3",
+    4: "space-x-4",
+    5: "space-x-5",
+    6: "space-x-6",
+};
+
 export const Breadcrumb = ({ className, spacing = 2, separator = "/", children }) => {
     const count = React.Children.count(children) - 1;
     return (
-        <div className={(className, clsx(`space-x-${spacing}`))}>
+        <div className={(className, sizes[spacing])}>
             {React.Children.map(children, (child, idx) => {
                 const isLast = idx >= count;
                 return (
