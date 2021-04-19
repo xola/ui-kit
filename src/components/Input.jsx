3f78e9a62a955c553e9e100a84d9eeaac95fec84
Nemanja K <nemanja.k@xola.com>
2021-04-19 20:46:03 +0200

    XOL-5923 Fix input focus border

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -15,7 +15,9 @@ export const Input = ({ size = "medium", className, type = "text", disabled, err
             className={clsx(
                 className,
                 sizes[size],
-                error ? "border-danger" : "border-gray-light",
+                error
+                    ? "border-danger focus:ring-danger focus:border-danger"
+                    : "border-gray-light focus:ring-primary focus:border-primary",
                 "block w-full placeholder-gray-dark text-gray-darker rounded-md leading-none",
                 disabled && "bg-gray-lighter",
             )}
