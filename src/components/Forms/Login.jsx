f292bcdb799235a7c77957eeb17e41ad79f8e059
Rushi Vishavadia <rushi@xola.com>
2021-04-21 20:00:40 +0530

    XOL-5923 Responsiveness

--- src/components/Forms/Login.jsx
+++ src/components/Forms/Login.jsx
@@ -23,19 +23,19 @@ export const Login = ({ className, onSubmit, ...rest }) => {
         <div className="flex flex-col h-screen bg-opacity-40 bg-blue-lighter">
             <div className="flex-grow">
                 <div className="flex flex-col items-center justify-center w-full h-full space-y-8">
-                    <div className="">
-                        <div className="mb-10">
+                    <div className="w-11/12 md:w-2/5">
+                        <div className="mb-4 md:mb-10">
                             <img
-                                className="w-auto h-12 mx-auto"
+                                className="w-auto h-10 mx-auto md:h-12"
                                 src="https://www.xola.com/images/xola-logo-black-small.png"
                                 alt="Xola Logo"
                             />
-                            <h2 className="mt-6 text-3xl font-bold text-center text-gray-darker">
+                            <h2 className="mt-6 text-xl font-bold text-center md:text-3xl text-gray-darker">
                                 Sign in to your account
                             </h2>
                         </div>
                         {/* TODO Form */}
-                        <form className="mt-8 space-y-6" onSubmit={onClick}>
+                        <form className="space-y-6" onSubmit={onClick}>
                             <div className="-space-y-px rounded-md shadow-sm">
                                 <div>
                                     {/* Change component */}
@@ -45,7 +45,7 @@ export const Login = ({ className, onSubmit, ...rest }) => {
                                         type="email"
                                         autoComplete="email"
                                         required
-                                        className="relative block w-full px-3 py-2 border rounded-none appearance-none border-gray-dark text-gray-darker placeholder-dark rounded-t-md focus:outline-none focus:ring-primary focus:border-blue-dark focus:z-10 sm:text-sm"
+                                        className="relative block w-full px-3 py-1 text-sm border rounded-none appearance-none md:px-3 md:py-2 border-gray-dark text-gray-darker placeholder-dark rounded-t-md focus:outline-none focus:ring-primary focus:border-blue-dark focus:z-10 sm:text-sm"
                                         placeholder="Email address"
                                     />
                                 </div>
@@ -57,7 +57,7 @@ export const Login = ({ className, onSubmit, ...rest }) => {
                                         type="password"
                                         autoComplete="current-password"
                                         required
-                                        className="relative block w-full px-3 py-2 border rounded-none appearance-none border-gray-dark placeholder-darker text-gray-darker rounded-b-md focus:outline-none focus:ring-primary focus:blue-dark focus:z-10 sm:text-sm"
+                                        className="relative block w-full px-3 py-1 text-sm border rounded-none appearance-none y-1 md:px-3 md:py-2 border-gray-dark placeholder-darker text-gray-darker rounded-b-md focus:outline-none focus:ring-primary focus:blue-dark focus:z-10 sm:text-sm"
                                         placeholder="Password"
                                     />
                                 </div>
@@ -101,19 +101,20 @@ export const Login = ({ className, onSubmit, ...rest }) => {
             </div>
             <footer className="">
                 <div className="px-4 py-1 mx-auto overflow-hidden max-w-7xl sm:px-6 lg:px-8">
-                    <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+                    <nav className="flex-wrap justify-center hidden -mx-5 -my-2 md:flex">
                         {links.map((link) => (
-                            <div key={link.name} className="px-5 py-2">
-                                <a href={link.href} className="text-base text-gray-dark hover:text-gray-darker">
+                            <div key={link.name} className="px-2 py-1 md:px-5 md:py-2">
+                                <a href={link.href} className="text-sm md:text-base text-gray-dark hover:text-gray-darker">
                                     {link.name}
                                 </a>
                             </div>
                         ))}
                     </nav>
-                    <p className="m-4 text-sm text-center text-gray">
+                    <p className="m-4 text-xs text-center md:text-sm text-gray">
                         <a href="https://www.xola.com/team" target="blank" className="">
-                            Handcrafted in Houston, Belgrade & Bengaluru &nbsp;&nbsp; &copy; {new Date().getFullYear()}{" "}
-                            Xola, Inc. <br />
+                            Handcrafted in Houston, Belgrade & Bengaluru &nbsp;&nbsp;
+                            <div className="md:hidden" />
+                            &copy; {new Date().getFullYear()} Xola, Inc. <br />
                         </a>
                     </p>
                 </div>
