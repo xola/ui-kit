d85ab0e26d316c9fe9155d05afb895316c7bfc6f
Rushi Vishavadia <rushi@xola.com>
2021-04-26 17:29:51 +0530

    Change sidebar breakpoints after testing for tablet

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,17 +10,17 @@ export const Sidebar = ({ children, footer, notifications, fixed }) => {
         <div
             className={clsx(
                 fixed ? "fixed" : "relative",
-                "w-24 md:w-56 h-screen p-2 overflow-y-auto bg-black text-white",
+                "w-24 lg:w-56 h-screen p-2 overflow-y-auto bg-black text-white",
             )}
         >
             {notifications ? (
-                <div className="p-2 text-center md:text-left">
+                <div className="p-2 text-center lg:text-left">
                     <NotificationCount className="text-sm">{notifications}</NotificationCount>
                 </div>
             ) : null}
 
             <div className="mb-10 text-center">
-                <XolaLogoCircle className="inline-block w-12 h-12 md:w-24 md:h-24" />
+                <XolaLogoCircle className="inline-block w-12 h-12 lg:w-24 lg:h-24" />
             </div>
 
             <div className="space-y-2">{children}</div>
@@ -33,7 +33,7 @@ Sidebar.Link = ({ active, icon: Icon, children, size = "small", ...rest }) => {
     return (
         <button
             className={clsx(
-                "transition-colors leading-none flex items-center justify-center md:justify-start w-full md:px-6 py-3 rounded",
+                "transition-colors leading-none flex items-center justify-center lg:justify-start w-full lg:px-6 py-3 rounded",
                 {
                     "bg-primary text-white hover:bg-primary-dark": active,
                     "hover:bg-gray-darker text-gray": !active,
@@ -41,9 +41,9 @@ Sidebar.Link = ({ active, icon: Icon, children, size = "small", ...rest }) => {
             )}
             {...rest}
         >
-            <Icon className="w-5 h-5 md:mr-3" />
-            <span className="hidden md:inline px-1">{children}</span>
-            <ChevronRightIcon className="hidden md:inline w-3 h-3 ml-auto" />
+            <Icon className="w-5 h-5 lg:mr-3" />
+            <span className="hidden px-1 lg:inline">{children}</span>
+            <ChevronRightIcon className="hidden w-3 h-3 ml-auto lg:inline" />
         </button>
     );
 };
@@ -54,7 +54,7 @@ Sidebar.Footer = ({ name }) => {
     return (
         <div className="absolute bottom-0 left-0 w-full px-6 pb-6">
             <div className="pt-3 border-t border-secondary-darker">
-                <Avatar className="mr-2" size="small" name={name} /> <span className="hidden md:inline">{name}</span>
+                <Avatar className="mr-2" size="small" name={name} /> <span className="hidden lg:inline">{name}</span>
             </div>
         </div>
     );
