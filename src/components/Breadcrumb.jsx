43a9757d75bd1be56c7f0c08143fba77eaab43e7
Nemanja K <nemanja.k@xola.com>
2021-04-27 13:00:20 +0200

    XOL-5923 Toolbar refactor

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,27 +1,21 @@
-import React from "react";
 import clsx from "clsx";
+import React, { Children } from "react";
 
-// This is required otherwise JIT won't pickup the dynamically generated classes like "space-x-2"
-const sizes = {
-    1: "space-x-1",
-    1.5: "space-x-1.5",
-    2: "space-x-2",
-    3: "space-x-3",
-    4: "space-x-4",
-    5: "space-x-5",
-    6: "space-x-6",
-};
+export const Breadcrumb = ({ className, classNames = {}, separator = "/", children, ...rest }) => {
+    const count = Children.count(children) - 1;
 
-export const Breadcrumb = ({ className, spacing = 2, separator = "/", children }) => {
-    const count = React.Children.count(children) - 1;
     return (
-        <div className={(className, sizes[spacing])}>
-            {React.Children.map(children, (child, idx) => {
-                const isLast = idx >= count;
+        <div className={clsx(className, "whitespace-nowrap font-semibold")} {...rest}>
+            {Children.map(children, (child, index) => {
+                const isLast = index >= count;
+
                 return (
                     <>
-                        <span className={clsx(isLast ? "text-black font-semibold" : "text-gray")}>{child}</span>
-                        <span className={clsx(className, { hidden: isLast }, "text-gray-dark")}>{separator}</span>
+                        <span className={clsx(classNames.item, isLast ? "text-black" : "text-gray")}>{child}</span>
+
+                        {isLast ? null : (
+                            <span className={clsx(classNames.separator, "text-gray-dark mx-2")}>{separator}</span>
+                        )}
                     </>
                 );
             })}
