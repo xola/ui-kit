d050ee5b84bfb5a77bc518feb5c4ca73ee86e419
Nemanja K <nemanja.k@xola.com>
2021-04-27 09:51:56 +0200

    XOL-5923 Refactor getInitials function

--- src/helpers/avatar.js
+++ src/helpers/avatar.js
@@ -1,13 +1,12 @@
 export const getInitials = (name) => {
     let initials = "N/A";
+    const pieces = typeof name === "string" && name.match(/\b\w/g);
 
-    if (typeof name === "string" && name.trim().length > 0) {
-        const sanitizedName = name.replace(/[^a-z0-9\s]/i, "").trim();
-        const pieces = sanitizedName.split(" ");
-        initials = pieces[0].slice(0, 1);
+    if (pieces) {
+        initials = pieces[0];
 
         if (pieces.length > 1) {
-            initials += pieces[pieces.length - 1].slice(0, 1);
+            initials += pieces[pieces.length - 1];
         }
     }
 
