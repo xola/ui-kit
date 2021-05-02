860fea6faaf86ac8881ebe3e2c2bba9b9c02b7ed
Rushi Vishavadia <rushi@xola.com>
2021-05-02 22:08:48 +0530

    XOL-5923 Use passed in locale

--- src/components/Utilities/Number.jsx
+++ src/components/Utilities/Number.jsx
@@ -4,7 +4,7 @@ import { format, roundNumber } from "../../helpers/numbers";
 
 const userLocale = getUserLocale();
 
-export const Number = ({ maximumFractionDigits = 2, children }) => {
-    const formattedNumber = format(children, null, userLocale, maximumFractionDigits);
+export const Number = ({ locale = userLocale, maximumFractionDigits = 2, children }) => {
+    const formattedNumber = format(children, null, locale, maximumFractionDigits);
     return <span className="formatted-number">{formattedNumber}</span>;
 };
