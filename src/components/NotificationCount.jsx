a8450bf534178098d655c9089a0ddd898685ace4
Rushi Vishavadia <rushi@xola.com>
2021-05-04 10:24:18 +0530

    XOL-5923 NotificationCount - Put className at end so it can be overriden

--- src/components/NotificationCount.jsx
+++ src/components/NotificationCount.jsx
@@ -5,8 +5,8 @@ export const NotificationCount = ({ className, ...rest }) => {
     return (
         <span
             className={clsx(
-                className,
                 "px-2 py-1 font-semibold inline-flex items-center justify-center rounded-full bg-red-dark text-white leading-none",
+                className,
             )}
             {...rest}
         />
