bc1ccdc616e456df3404e826eb3fad2317b49a97
Nemanja K <nemanja.k@xola.com>
2021-05-04 12:35:35 +0200

    XOL-5923 Add className prop to Tooltip

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -3,11 +3,10 @@ import Tippy from "@tippyjs/react";
 import { followCursor } from "tippy.js"; // Dont remove this even if unused. It is required for one prop
 import "tippy.js/dist/tippy.css"; // If we customize the style, the change this and import our own style
 
-export const Tooltip = ({ children, ...rest }) => {
+export const Tooltip = ({ children, className, ...rest }) => {
     return (
         <Tippy {...rest} className="text-white xola-tooltip" plugins={[followCursor]}>
-            {/* If you put a Badge inside the Tooltip it will cause a layout issue. To be fixed */}
-            <span>{children}</span>
+            <span className={className}>{children}</span>
         </Tippy>
     );
 };
