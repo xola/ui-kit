2beeb7ef33c3ddd52e4dd0c7ad813a77e049e62b
Rushi Vishavadia <rushi@xola.com>
2021-05-18 22:24:23 +0530

    XOL-5923 Add additional line height option

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -3,6 +3,7 @@ import Tippy from "@tippyjs/react";
 import { followCursor } from "tippy.js"; // Dont remove this even if unused. It is required for one prop
 import "tippy.js/dist/tippy.css"; // If we customize the style, the change this and import our own style
 
+// TODO: Implement "as='div'"
 export const Tooltip = ({ children, className, ...rest }) => {
     return (
         <Tippy {...rest} className="text-white xola-tooltip" plugins={[followCursor]}>
