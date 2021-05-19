f347502d95d26cf00f07f8398bfce0590a62b47c
Nemanja K <nemanja.k@xola.com>
2021-05-19 22:39:07 +0200

    XOL-5923 Fix checkbox id changing on every render

--- src/components/Forms/Checkbox.jsx
+++ src/components/Forms/Checkbox.jsx
@@ -1,14 +1,16 @@
 import uniqueId from "lodash/uniqueId";
-import React from "react";
+import React, { useState } from "react";
 
-const Checkbox = ({ label, id = uniqueId("checkbox-"), ...rest }) => {
+const Checkbox = ({ label, ...rest }) => {
+    const [id] = useState(rest.id ?? uniqueId("checkbox-"));
+
     return (
         <div className="flex items-center">
             <input
-                id={id}
                 type="checkbox"
                 className="w-4 h-4 rounded text-primary border-gray focus:primary disabled:bg-gray-lighter"
                 {...rest}
+                id={id}
             />
 
             <label htmlFor={id} className="ml-2 text-gray-darker leading-none">
