db53fdf90d5ff8c9af8bb6e621ab14b92e764bc1
Rushi Vishavadia <rushi@xola.com>
2021-05-21 21:35:50 +0530

    XOL-5923 Fix negative & positive margin offset that caused overflows

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,8 +3,8 @@ import React, { Children, cloneElement } from "react";
 
 export const Table = ({ className, ...rest }) => (
     <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        <div className="-my-2 overflow-x-auto">
+            <div className="inline-block min-w-full py-2 align-middle">
                 <div className="overflow-hidden border-b border-gray-lighter sm:rounded-lg">
                     <table className={clsx(className, "min-w-full divide-y border border-gray-lighter")} {...rest} />
                 </div>
