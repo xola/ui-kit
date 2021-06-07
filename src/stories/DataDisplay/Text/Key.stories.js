233e6cb3329d4bdd6d436d7f4817a1751a603d6b
Rushi Vishavadia <rushi@xola.com>
2021-06-07 18:41:25 +0530

    Further breakdown

---
+++ src/stories/DataDisplay/Text/Key.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Key } from "../..";
+
+export default {
+    title: "Data Display/Text/Key",
+    component: Key,
+    parameters: {
+        docs: {
+            description: {
+                component: "Display a character as a keyboard key",
+            },
+        },
+    },
+    argTypes: {
+        char: {
+            description: "Character t show",
+            type: { required: true },
+            control: { type: "text" },
+            table: {
+                type: { summary: "example: K" },
+            },
+        },
+    },
+};
+
+export const Default = ({ char = "K" }) => {
+    return <Key char={char} />;
+};
+
+export const MacCommandKey = () => {
+    return <Key char="cmd" />;
+};
+
+export const SpecialKeys = () => {
+    return (
+        <div class="space-x-3">
+            <Key char="shift" />
+            <Key char="esc" />
+            <Key char="alt" />
+            <Key char="enter" />
+        </div>
+    );
+};
---
+++ src/stories/Screens/Login.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Login } from "../..";
+
+export default {
+    title: "Screens/Login",
+    component: Login,
+    parameters: {
+        docs: {
+            description: {
+                component: "A default login screen specifically for Xola",
+            },
+        },
+    },
+};
+
+const onSubmit = (email, password) => {
+    console.log("arguments", arguments);
+    alert(`Hello ${email} Password: ${password.slice(0, 2)}*** This is a WIP`);
+};
+
+export const Default = () => {
+    return <Login onSubmit={onSubmit} />;
+};
