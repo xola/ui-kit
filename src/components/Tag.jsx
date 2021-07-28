8adbd0cb513a9e6cfece8b85b99d85f7199266d6
Rushi Vishavadia <rushi@xola.com>
2021-07-28 16:14:07 +0530

    X2-16 Test publish action

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -17,8 +17,6 @@ const sizes = {
     medium: "px-2 py-1 text-base",
 };
 
-// Dashboard - height 25 Padding 6, 8Purchases - 20 padding 3,4
-
 export const Tag = ({
     color = "primary",
     size = "small",
