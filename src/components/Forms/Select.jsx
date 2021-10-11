e2fe63e05eddf0b5eb8f054280b5b44b545511cf
Nemanja Krstić <nemanja.k@xola.com>
2021-10-11 13:53:20 +0200

    Create and style Select component with react-select (#57)
    Co-authored-by: Rushi Vishavadia <rushi@xola.com>

--- src/components/Forms/Select.jsx
+++ src/components/Forms/Select.jsx
@@ -1,15 +1,21 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import React from "react";
-import { BaseInput } from "./BaseInput";
+import ReactSelect from "react-select";
+import "./Select.css";
 
-// TODO: Use `isNative` after implementing custom options.
-// eslint-disable-next-line no-unused-vars
-export const Select = ({ className, isNative = false, ...rest }) => {
-    return <BaseInput className={clsx("ui-select", className)} as="select" {...rest} />;
+export const Select = ({ className, isSearchable = false, ...rest }) => {
+    return (
+        <ReactSelect
+            className={clsx("ui-select", className)}
+            isSearchable={isSearchable}
+            classNamePrefix="ui-select"
+            {...rest}
+        />
+    );
 };
 
 Select.propTypes = {
-    ...BaseInput.propTypes,
-    isNative: PropTypes.bool,
+    className: PropTypes.string,
+    isSearchable: PropTypes.bool,
 };
