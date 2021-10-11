8dcb57e1183674d39c36d331d60a6b69b9812f7b
Nemanja Krstić <nemanja.k@xola.com>
2021-10-11 13:51:31 +0200

    Lint fixes

--- src/stories/Forms/Select.stories.js
+++ src/stories/Forms/Select.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Select, Label, FormGroup, Currency } from "../..";
 import clsx from "clsx";
+import { Select, Label, FormGroup, Currency } from "../..";
 
 const SelectStories = {
     primary: true,
@@ -77,12 +77,12 @@ export const RenderCustomOptionItems = () => {
 
     const Option = ({ innerProps, innerRef, label, data, isSelected, isFocused }) => (
         <div
+            ref={innerRef}
             className={clsx(
                 "flex items-center p-4 border-b border-gray-light",
                 isSelected && "text-white bg-success-dark",
                 !isSelected && isFocused && "bg-success-lighter",
             )}
-            ref={innerRef}
             {...innerProps}
         >
             <div className="mr-auto">
