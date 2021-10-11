35bc90409b487a29da5b27c146f6d4f4a9aaaf89
Nemanja Krstić <nemanja.k@xola.com>
2021-10-11 18:10:44 +0200

    X2-143 Fixes

--- src/components/Breakdown.jsx
+++ src/components/Breakdown.jsx
@@ -2,6 +2,8 @@ import clsx from "clsx";
 import React from "react";
 
 const colors = {
+    default: "text-gray-darker",
+    black: "text-black",
     primary: "text-primary",
     secondary: "text-secondary",
     warning: "text-warning",
@@ -10,15 +12,19 @@ const colors = {
     caution: "text-caution",
 };
 
-export const Breakdown = ({ children }) => {
-    return <table className="w-full text-gray-darker">{children}</table>;
+export const Breakdown = ({ children, className, ...rest }) => {
+    return (
+        <table className={clsx("ui-breakdown", "w-full", className)} {...rest}>
+            <tbody>{children}</tbody>
+        </table>
+    );
 };
 
-const BreakdownItem = ({ children, value, icon, ...rest }) => {
+const BreakdownItem = ({ children, info, value, className, color = "default", ...rest }) => {
     return (
-        <tr {...rest}>
+        <tr className={clsx("ui-breakdown-item", colors[color], className)} {...rest}>
             <td className="text-left">{children}</td>
-            <td className="text-right">{icon}</td>
+            <td className="text-right">{info}</td>
 
             <td style={{ width: "1%" }} className="pl-4 text-right">
                 {value}
@@ -30,12 +36,11 @@ const BreakdownItem = ({ children, value, icon, ...rest }) => {
 Breakdown.Item = BreakdownItem;
 Breakdown.Item.displayName = "Breakdown.Item";
 
-const BreakdownSubtotalItem = ({ className, icon, children, value }) => {
+const BreakdownSubtotalItem = ({ children, info, value, className, color = "black", ...rest }) => {
     return (
-        <tr className={clsx("font-bold text-black", className)}>
-            <td colSpan={2} className="pt-1 pb-4 text-right">
-                {children}
-            </td>
+        <tr className={clsx("ui-breakdown-subtotal-item", "font-bold", colors[color], className)} {...rest}>
+            <td className="pt-1 pb-4 text-left">{children}</td>
+            <td className="pt-1 pb-4 text-right">{info}</td>
 
             <td style={{ width: "1%" }} className="pt-1 pb-4 pl-4 text-right">
                 {value}
@@ -47,9 +52,9 @@ const BreakdownSubtotalItem = ({ className, icon, children, value }) => {
 Breakdown.SubtotalItem = BreakdownSubtotalItem;
 Breakdown.SubtotalItem.displayName = "Breakdown.SubtotalItem";
 
-const BreakdownSeparator = () => {
+const BreakdownSeparator = ({ className, ...rest }) => {
     return (
-        <tr>
+        <tr className={clsx("ui-breakdown-separator", className)} {...rest}>
             <td colSpan={3} className="pb-1 border-b border-gray-light" />
         </tr>
     );
--- src/stories/Breakdown.stories.js
+++ src/stories/Breakdown.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Breakdown, CardIcon } from "..";
+import { Breakdown, CardIcon, Button } from "..";
 
 const HeaderToolbarStories = {
     title: "Other/Breakdown",
@@ -14,19 +14,32 @@ export const Default = () => {
                 <Breakdown.Item value="$29.00">Adults ($29.00 x 1)</Breakdown.Item>
                 <Breakdown.Item value="$100.00">Charge</Breakdown.Item>
                 <Breakdown.Item value="$4.00">VAT ($2.00 x 2)</Breakdown.Item>
+
                 <Breakdown.Separator />
-                <Breakdown.SubtotalItem value="$162.00">Total</Breakdown.SubtotalItem>
-                <Breakdown.Item value="$62.00" icon={<CardIcon />}>
+
+                <Breakdown.SubtotalItem info="Total" value="$162.00">
+                    <Button color="outline" size="small">
+                        Modify Taxes &amp; Fees
+                    </Button>
+                </Breakdown.SubtotalItem>
+
+                <Breakdown.Item value="$62.00" info={<CardIcon />}>
                     Payment (12/18/2019)
                 </Breakdown.Item>
-                <Breakdown.Item className="text-primary" value="-$62.00" icon={<CardIcon />}>
+
+                <Breakdown.Item color="primary" value="-$62.00" info={<CardIcon />}>
                     Refund (07/23/2021)
                 </Breakdown.Item>
+
                 <Breakdown.Separator />
-                <Breakdown.SubtotalItem value="$62.00">Paid</Breakdown.SubtotalItem>
-                <Breakdown.SubtotalItem className="text-danger" value="$62.00">
-                    Balance
+
+                <Breakdown.SubtotalItem info="Paid" value="$62.00">
+                    <Button color="outline" size="small">
+                        Apply Code
+                    </Button>
                 </Breakdown.SubtotalItem>
+
+                <Breakdown.SubtotalItem info="Balance" color="danger" value="$62.00" />
             </Breakdown>
         </div>
     );
