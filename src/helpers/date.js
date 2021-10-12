78856dc1266f729f88cad7ec2e33f824b6804660
Rushi Vishavadia <rushi@xola.com>
2021-10-12 10:04:57 +0530

    X2-157 Helper method for formatting time

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,5 +1,12 @@
 import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
 
 export const formatDate = (date, format = "YYYY-MM-DD") => {
     return dayjs(date).format(format);
+};
+
+export const formatTime = (time, format = "h:mm a") => {
+    const strTime = String(time).padStart(4, 0); // 700 to 0700
+    return dayjs(strTime, "hhmm").format(format);
 };
