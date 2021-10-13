974da78aecce110291497a8c599d3df75adfb78b
Nemanja Krstić <nemanja.k@xola.com>
2021-10-13 19:37:18 +0200

    ComboBox fixes

--- src/components/Forms/ComboBox.jsx
+++ src/components/Forms/ComboBox.jsx
@@ -4,10 +4,17 @@ import React from "react";
 import Select from "react-select";
 import "./ComboBox.css";
 
-export const ComboBox = ({ className, ...rest }) => {
-    return <Select className={clsx("ui-combo-box", className)} classNamePrefix="ui-combo-box" {...rest} />;
+export const ComboBox = ({ className, isError, ...rest }) => {
+    return (
+        <Select
+            className={clsx("ui-combo-box", isError && "border-danger", className)}
+            classNamePrefix="ui-combo-box"
+            {...rest}
+        />
+    );
 };
 
 ComboBox.propTypes = {
     className: PropTypes.string,
+    isError: PropTypes.bool,
 };
