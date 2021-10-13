b71d49395e87a4c9c0f1a1e1deb769349acd7cdc
Rushi Vishavadia <rushi@xola.com>
2021-10-13 18:45:26 +0530

    X2-157 Switch month year selector to use native select

--- src/components/DatePicker/MonthYearSelector.jsx
+++ src/components/DatePicker/MonthYearSelector.jsx
@@ -9,46 +9,32 @@ export const MonthYearSelector = ({ date, onChange }) => {
     const months = [...Array.from({ length: 12 }).keys()].map((m) => today.month(m).format("MMM"));
     const years = [...Array.from({ length: 12 }).keys()].map((y) => today.year(2021 + y).format("YYYY"));
 
-    const handleMonthChange = (data) => {
-        // console.log("handle month", data.label, data.value);
-        date.setMonth(data.value);
-        onChange(date);
+    const handleChange = (event) => {
+        const { year, month } = event.target.form;
+        onChange(new Date(year.value, month.value));
     };
-
-    const handleYearChange = (data) => {
-        // console.log("handle year", data.label, data.value);
-        date.setYear(data.value);
-        onChange(date);
-    };
-
-    const monthOptions = months.map((month, index) => {
-        return { label: month, value: index, isOptionSelected: true };
-    });
-    const yearOptions = years.map((year) => {
-        return { label: year, value: Number(year) };
-    });
 
     return (
-        <form className="space-x-2 DayPicker-Caption">
-            <Select
-                name="month"
-                value={monthOptions[date.getMonth()]}
-                options={monthOptions}
-                className="inline-block w-20 month-selector"
-                onChange={handleMonthChange}
-                styles={{ menuPortal: (base) => ({ ...base, zIndex: 30 }) }}
-                menuPortalTarget={document.body}
-            />
+        <form className="DayPicker-Caption space-x-2">
+            <span className="inline-block">
+                <Select name="month" value={date.getMonth()} className="month-selector" onChange={handleChange}>
+                    {months.map((month, index) => (
+                        <option key={month} value={index}>
+                            {month}
+                        </option>
+                    ))}
+                </Select>
+            </span>
 
-            <Select
-                name="year"
-                options={yearOptions}
-                value={yearOptions.find((y) => y.value === date.getFullYear())}
-                className="inline-block w-22 year-selector"
-                onChange={handleYearChange}
-                styles={{ menuPortal: (base) => ({ ...base, zIndex: 30 }) }}
-                menuPortalTarget={document.body}
-            />
+            <span className="inline-block">
+                <Select name="year" value={date.getFullYear()} className="year-selector" onChange={handleChange}>
+                    {years.map((year) => (
+                        <option key={year} value={year}>
+                            {year}
+                        </option>
+                    ))}
+                </Select>
+            </span>
         </form>
     );
 };
