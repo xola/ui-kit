87707f53a4316de88d035a172d073bc5a4949a51
Rushi Vishavadia <rushi@xola.com>
2021-10-17 18:31:07 +0530

    Uprgade skeleton to better handle classes

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -3,19 +3,19 @@ import PropTypes from "prop-types";
 import React from "react";
 import styles from "./Skeleton.module.css";
 
-export const Skeleton = ({ className, style, height = 300, shouldAnimate = true, children, ...rest }) => {
+export const Skeleton = ({ style, height = 300, shouldAnimate = true, children, classes = {}, ...rest }) => {
     return (
         <div
             className={clsx(
                 "ui-skeleton",
-                className,
                 "relative flex items-center justify-center bg-gray-lighter overflow-hidden rounded border border-gray-lighter",
+                classes.container,
             )}
             style={{ height, ...style }}
             {...rest}
         >
-            {shouldAnimate ? <div className={clsx(styles.shimmer, "absolute w-full h-full")} /> : null}
-            <div className="text-gray">{children}</div>
+            {shouldAnimate ? <div className={clsx(styles.shimmer, "absolute w-full h-full", classes.shimmer)} /> : null}
+            <div className={clsx("text-gray", classes.text)}>{children}</div>
         </div>
     );
 };
