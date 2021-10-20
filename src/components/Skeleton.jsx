39fef34e60d2b74aff79cd6c03247237702ec351
Rushi Vishavadia <rushi@xola.com>
2021-10-20 16:14:46 +0530

    Rename prop to classnames

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -3,19 +3,19 @@ import PropTypes from "prop-types";
 import React from "react";
 import styles from "./Skeleton.module.css";
 
-export const Skeleton = ({ style, height = 300, shouldAnimate = true, children, classes = {}, ...rest }) => {
+export const Skeleton = ({ style, height = 300, shouldAnimate = true, children, classNames = {}, ...rest }) => {
     return (
         <div
             className={clsx(
                 "ui-skeleton",
                 "relative flex items-center justify-center bg-gray-lighter overflow-hidden rounded border border-gray-lighter",
-                classes.container,
+                classNames.container,
             )}
             style={{ height, ...style }}
             {...rest}
         >
-            {shouldAnimate ? <div className={clsx(styles.shimmer, "absolute w-full h-full", classes.shimmer)} /> : null}
-            <div className={clsx("text-gray", classes.text)}>{children}</div>
+            {shouldAnimate ? <div className={clsx(styles.shimmer, "absolute w-full h-full", classNames.shimmer)} /> : null}
+            <div className={clsx("text-gray", classNames.text)}>{children}</div>
         </div>
     );
 };
@@ -24,6 +24,6 @@ Skeleton.propTypes = {
     style: PropTypes.object,
     height: PropTypes.string,
     shouldAnimate: PropTypes.bool,
-    classes: PropTypes.object,
+    classNames: PropTypes.object,
     children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 };
