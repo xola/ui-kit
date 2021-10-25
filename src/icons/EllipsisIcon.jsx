c17c0a2e460725664a51b6039f61d5bc203a6577
Rushi Vishavadia <rushi@xola.com>
2021-10-25 09:22:04 +0530

    Fix EllipsisIcon

--- src/icons/EllipsisIcon.jsx
+++ src/icons/EllipsisIcon.jsx
@@ -3,8 +3,8 @@ import { createIcon } from "../helpers/icon";
 
 export const EllipsisIcon = createIcon((props) => {
     return (
-        <svg viewBox="0 0 14 14" width={15} height={14} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-            <g clipPath="url(#prefix__clip0)" fill="currentColor">
+        <svg viewBox="0 0 14 14" width={14} height={14} xmlns="http://www.w3.org/2000/svg" {...props}>
+            <g fill="currentColor">
                 <path d="M14.166 7a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0zM4.166 7a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0zM9.166 7a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z" />
             </g>
             <defs>
