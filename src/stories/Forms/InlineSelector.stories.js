af9364c414ed00ef75b4e790e93ba2167e8ff9ea
Rushi Vishavadia <rushi@xola.com>
2021-12-03 13:05:37 +0530

    Simple inline selector

---
+++ src/stories/Forms/InlineSelector.stories.js
@@ -0,0 +1,42 @@
+import { random } from "lodash";
+import React, { useState, useRef } from "react";
+import { InlineSelector, Button, Input, Select, FormGroup } from "../..";
+
+const InlineSelectorStories = {
+    title: "Forms & Fields/Inline Selector",
+    component: InlineSelector,
+};
+
+export const Default = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [text, setText] = useState("8 hours");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setText(`${random(1, 10)} hours`);
+        setIsOpen(false);
+    };
+
+    return (
+        <div>
+            <div className="mb-5">
+                Values are randomly set just for story purposes. Use your form library to fetch form values and set them
+                properly
+            </div>
+            <InlineSelector text={text} isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} className="text-blue">
+                <form onSubmit={handleSubmit}>
+                    <FormGroup className="flex flex-row space-x-2">
+                        <Input />
+                        <Select>
+                            <option value="hours">hours</option>
+                            <option value="minutes">minutes</option>
+                        </Select>
+                        <Button>Apply</Button>
+                    </FormGroup>
+                </form>
+            </InlineSelector>
+        </div>
+    );
+};
+
+export default InlineSelectorStories;
