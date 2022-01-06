e254782713a5409d04a2c95890ed9d0b7c111870
Salewan <salewan@gmail.com>
2022-01-06 13:37:21 +0700

    X2-756 User is able to upload image more than 5 mb size

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,9 +1,19 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
+import filesize from "filesize";
 import { Logo, Button, Spinner, ImageIcon, TrashIcon } from "..";
 
-export const ImageUpload = ({ src, size = "large", onChange, onDelete, isLoading = false, ...props }) => {
+export const ImageUpload = ({
+    src,
+    size = "large",
+    onChange,
+    onDelete,
+    onError,
+    isLoading = false,
+    maxSize = 5_000_000,
+    ...props
+}) => {
     const inputReference = useRef();
 
     const handleUploadClick = () => {
@@ -12,6 +22,12 @@ export const ImageUpload = ({ src, size = "large", onChange, onDelete, isLoading
 
     const handleChange = (event) => {
         const [file] = event.target.files;
+
+        if (maxSize && maxSize < file.size) {
+            onError(`Image size shouldn't exceed ${filesize(maxSize)}`);
+            return;
+        }
+
         onChange(file);
     };
 
@@ -52,7 +68,8 @@ export const ImageUpload = ({ src, size = "large", onChange, onDelete, isLoading
                     {isLoading && <Spinner />}
                 </div>
                 <div className="text-xs text-gray-darker">
-                    Check that the image is in PNG or JPG format and does not exceed 5 MB
+                    Check that the image is in PNG or JPG format
+                    {maxSize ? ` and does not exceed ${filesize(maxSize)}` : ""}
                 </div>
             </div>
         </div>
@@ -64,5 +81,7 @@ ImageUpload.propTypes = {
     size: PropTypes.oneOf(["small", "medium", "large"]),
     onChange: PropTypes.func.isRequired,
     onDelete: PropTypes.func.isRequired,
+    onError: PropTypes.func.isRequired,
     isLoading: PropTypes.bool,
+    maxSize: PropTypes.number,
 };
