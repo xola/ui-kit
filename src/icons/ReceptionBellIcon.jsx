55bdc028d57fa23323a24001e0fa9099815e4594
Rushi Vishavadia <rushi@xola.com>
2022-01-27 14:00:07 +0530

    Lint fix

--- src/icons/ReceptionBellIcon.jsx
+++ src/icons/ReceptionBellIcon.jsx
@@ -10,7 +10,7 @@ export const ReceptionBellIcon = createIcon((props) => {
                 strokeLinejoin="round"
                 strokeWidth="1.3"
                 d="M9.715 3.857v3.568M8.429 3.857H11M3.535 14.56a.893.893 0 01-.884-1.018 7.135 7.135 0 0114.125 0 .894.894 0 01-.882 1.02l-12.36-.002z"
-            ></path>
+            />
         </svg>
     );
 });
