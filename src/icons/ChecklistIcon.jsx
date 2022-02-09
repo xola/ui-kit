056a849bc65001fb26558b617837545e0ce08980
Rushi Vishavadia <rushi@xola.com>
2022-02-09 14:12:11 +0530

    X2-1024 Fix manually migrated icons

--- src/icons/ChecklistIcon.jsx
+++ src/icons/ChecklistIcon.jsx
@@ -6,7 +6,7 @@ export const ChecklistIcon = createIcon((props) => {
         <svg width={14} height={14} fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" {...props}>
             <path
                 d="M2 4.638l.983.983L5.603 3M2 10.638l.983.983L5.603 9M7.57 4h3.93M7.57 10h3.93"
-                stroke="#222324"
+                stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
--- src/icons/SearchIcon.jsx
+++ src/icons/SearchIcon.jsx
@@ -6,7 +6,7 @@ export const SearchIcon = createIcon((props) => {
         <svg width={14} height={14} fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" {...props}>
             <path
                 d="M6.217 10.878a4.661 4.661 0 100-9.322 4.661 4.661 0 000 9.322zM9.514 9.513l2.93 2.931"
-                stroke="#222324"
+                stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
