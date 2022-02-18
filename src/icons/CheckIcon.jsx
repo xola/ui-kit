a9e8854f06682e88dd13a003571293d724ee5ac9
Rushi Vishavadia <rushi@xola.com>
2022-02-18 14:38:04 +0530

    Icon update

--- src/icons/CheckIcon.jsx
+++ src/icons/CheckIcon.jsx
@@ -3,7 +3,7 @@ import { createIcon } from "../helpers/icon";
 
 export const CheckIcon = createIcon((props) => {
     return (
-        <svg viewBox="0 0 14 13" width={14} height={14} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <svg viewBox="0 0 14 12" width={14} height={14} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
             <path d="M12.166 3l-7.333 7.333L1.5 7" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     );
