11e57edd2dcfb295066ff674898a5a42c75ace47
Rushi Vishavadia <rushi@xola.com>
2022-02-22 13:21:03 +0530

    Set stroke to currentColor

--- src/icons/DownArrowIcon.jsx
+++ src/icons/DownArrowIcon.jsx
@@ -6,7 +6,7 @@ export const DownArrowIcon = createIcon((props) => {
         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 14 14" {...props}>
             <path
                 d="M10 6L7.184 8.816v0a.26.26 0 01-.368 0L4 6"
-                stroke="#222324"
+                stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
--- src/icons/PartnerPackageIcon.jsx
+++ src/icons/PartnerPackageIcon.jsx
@@ -6,19 +6,19 @@ export const PartnerPackageIcon = createIcon((props) => {
         <svg viewBox="0 0 14 14" width={14} height={14} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
             <path
                 d="M.5 9.994h1.066a1.066 1.066 0 001.066-1.066V4.665a1.065 1.065 0 00-1.066-1.066H.506M13.284 3.6h-1.06a1.066 1.066 0 00-1.066 1.065v4.263a1.065 1.065 0 001.066 1.066h1.06"
-                stroke="#222324"
+                stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M11.175 9.112h-.688c-.228 0-.454.05-.662.144l-3.363 1.532M5.811 4.333a1.59 1.59 0 00-1.339-.135l-1.838.663M11.286 4.147L8.987 3.135a1.599 1.599 0 00-1.513.122L5.388 4.609a1.204 1.204 0 00-.154 1.901v0a1.205 1.205 0 001.317.2l1.244-.581"
-                stroke="#222324"
+                stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M7.794 6.128l.632.573a1.599 1.599 0 01.006 2.364L6.82 10.54a1.599 1.599 0 01-2.153.006L3.56 9.54a1.599 1.599 0 00-.948-.4"
-                stroke="#222324"
+                stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
