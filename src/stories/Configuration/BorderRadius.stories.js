064f8bfa3eb9804d71933bbd8b42314a8158dfad
Rushi Vishavadia <rushi@xola.com>
2022-03-05 19:56:34 +0530

    Bug fix for border radius

--- src/stories/Configuration/BorderRadius.stories.js
+++ src/stories/Configuration/BorderRadius.stories.js
@@ -1,4 +1,3 @@
-import clsx from "clsx";
 import React from "react";
 import twConfig from "../../../tailwind.config";
 
@@ -15,13 +14,16 @@ export const BorderRadius = () => {
         <div className="space-y-4">
             {keys.map((key) => {
                 const name = key === "DEFAULT" ? "" : `-${key}`;
-                const brClassName = `rounded${name}`;
                 return (
                     <div key={key} className="flex flex-row items-center font-mono text-sm">
-                        <span className="mr-1 min-w-24 shrink-0 text-gray-darker">rounded{name}</span>
+                        <span className="mr-1 min-w-24 shrink-0 text-gray-darker">
+                            rounded
+                            {name}
+                        </span>
                         <div
                             key={key}
-                            className={clsx("ml-2 flex h-10 w-72 items-center bg-blue-lighter pl-3", brClassName)}
+                            className="ml-2 flex h-10 w-72 items-center bg-blue-lighter pl-3"
+                            style={{ borderRadius: borderRadius[key] }}
                         >
                             border-radius: &quot;{borderRadius[key]}&quot;
                         </div>
