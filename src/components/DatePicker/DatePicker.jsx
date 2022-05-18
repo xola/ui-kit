bc66b30229c9b7273c16023237c941ee38d94f0b
Nemanja Krstić <nemanja.k@xola.com>
2022-05-18 15:55:56 +0200

    Fix console errors

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -132,5 +132,5 @@ DatePicker.propTypes = {
     modifiers: PropTypes.object,
     ranges: PropTypes.arrayOf(PropTypes.oneOf(["day", "week", "month", "quarter", "year"])),
     shouldShowRelativeRanges: PropTypes.bool,
-    components: PropTypes.shape({ Footer: PropTypes.node }),
+    components: PropTypes.shape({ Footer: PropTypes.oneOfType([PropTypes.node, PropTypes.func]) }),
 };
