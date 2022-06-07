4b3ec26c0527d1a18468831b4dced348f4d28c57
Ejaz Karim <ejazkarimhunzai@gmail.com>
2022-06-07 13:24:58 +0500

    Hide Today from range datepicker (#131)

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -95,7 +95,7 @@ export const DatePicker = ({
                     useDateRangeStyle ? "date-range-picker" : null,
                     getDayContent ? "has-custom-content" : null,
                 )}
-                todayButton="Today"
+                todayButton={variant === "single" && "Today"}
                 selectedDays={value}
                 month={currentMonth}
                 modifiers={{ ...modifiers, ...rangeModifier }}
