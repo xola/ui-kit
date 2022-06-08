3147750ee6130595aab128db7f4c69d0eca719db
Ejaz Karim <ejazkarimhunzai@gmail.com>
2022-06-08 10:35:23 +0500

    X2-1720 Date picker > Upcoming dates (#128)
    Co-authored-by: Nemanja Krstić <nemanja.k@xola.com>

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -30,6 +30,7 @@ export const DatePicker = ({
     ranges,
     shouldShowRelativeRanges = false,
     components = {},
+    upcomingDates,
     ...rest
 }) => {
     const initialValue = variant === variants.single ? value : value.from;
@@ -88,27 +89,66 @@ export const DatePicker = ({
 
     return (
         <>
-            <DayPicker
-                showOutsideDays
-                className={clsx(
-                    "ui-date-picker rounded-lg pt-3",
-                    useDateRangeStyle ? "date-range-picker" : null,
-                    getDayContent ? "has-custom-content" : null,
-                )}
-                todayButton={variant === "single" && "Today"}
-                selectedDays={value}
-                month={currentMonth}
-                modifiers={{ ...modifiers, ...rangeModifier }}
-                numberOfMonths={isRangeVariant ? 2 : 1}
-                disabledDays={disabledDays}
-                captionElement={captionElement}
-                renderDay={renderDay}
-                navbarElement={NavbarElement}
-                onDayClick={handleDayClick}
-                onMonthChange={handleMonthChange}
-                onTodayButtonClick={handleDayClick}
-                {...rest}
-            />
+            <div className="flex">
+                {upcomingDates ? (
+                    <div className="rounded-l-lg border-r border-gray pt-8">
+                        <p className="mb-2 px-6 text-lg font-bold">Upcoming</p>
+                        {upcomingDates?.length > 0 ? (
+                            <div className="mt-5">
+                                {upcomingDates?.map((date, index) => {
+                                    return (
+                                        <div
+                                            key={index.toString()}
+                                            value
+                                            className={clsx(
+                                                "mx-6 mt-3 flex min-w-[160px] cursor-pointer items-center justify-center rounded border border-gray py-3 hover:border-blue hover:bg-blue hover:text-white",
+                                                {
+                                                    "border-blue bg-blue text-white": dayjs(date).isSame(
+                                                        dayjs(value),
+                                                        "day",
+                                                    ),
+                                                },
+                                            )}
+                                            onClick={(event) => {
+                                                handleDayClick(date, {}, event);
+                                                handleMonthChange(date);
+                                            }}
+                                        >
+                                            {dayjs(date).format("ddd DD MMMM")}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        ) : (
+                            <div className="mx-6 mt-7 max-w-[160px] items-center justify-center rounded bg-yellow-lighter p-3">
+                                There is no future availability for this product.
+                            </div>
+                        )}
+                    </div>
+                ) : null}
+
+                <DayPicker
+                    showOutsideDays
+                    className={clsx(
+                        "ui-date-picker rounded-lg pt-3",
+                        useDateRangeStyle ? "date-range-picker" : null,
+                        getDayContent ? "has-custom-content" : null,
+                    )}
+                    todayButton={variant === "single" && "Today"}
+                    selectedDays={value}
+                    month={currentMonth}
+                    modifiers={{ ...modifiers, ...rangeModifier }}
+                    numberOfMonths={isRangeVariant ? 2 : 1}
+                    disabledDays={disabledDays}
+                    captionElement={captionElement}
+                    renderDay={renderDay}
+                    navbarElement={NavbarElement}
+                    onDayClick={handleDayClick}
+                    onMonthChange={handleMonthChange}
+                    onTodayButtonClick={handleDayClick}
+                    {...rest}
+                />
+            </div>
 
             {components.Footer ? <components.Footer /> : null}
 
@@ -124,6 +164,7 @@ export const DatePicker = ({
 DatePicker.propTypes = {
     variant: PropTypes.oneOf(Object.keys(variants)),
     value: PropTypes.objectOf(Date),
+    upcomingDates: PropTypes.arrayOf(Date),
     onChange: PropTypes.func.isRequired,
     onMonthChange: PropTypes.func,
     disabledDays: PropTypes.oneOfType([PropTypes.object, PropTypes.array, PropTypes.func]),
