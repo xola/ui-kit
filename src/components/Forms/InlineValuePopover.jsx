47904a2f08d1639f99734fb02ad78bbd4dd86d9c
Oleksandr Khomyakov <khomyakov@users.noreply.github.com>
2022-06-14 14:00:09 +0300

    Added an arrow to the Inline Value Popover to match the new figma design (#132)
    * Added an arrow to the Inline Value Popover to match the new figma design
    
    * Added optional arrow to InlineValuePopover
    
    * Arrow fix

--- src/components/Forms/InlineValuePopover.jsx
+++ src/components/Forms/InlineValuePopover.jsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
 import React from "react";
 import { Popover } from "../Popover/Popover";
+import { DownArrowIcon } from "../..";
 
 export const InlineValuePopover = ({
     text,
     isOpen = false,
+    showArrow = false,
     onClick,
     onClickOutside,
     classNames,
@@ -13,12 +15,11 @@ export const InlineValuePopover = ({
 }) => {
     return (
         <Popover visible={isOpen} onClickOutside={onClickOutside} {...rest}>
-            <span
-                className={clsx("cursor-pointer border-b border-primary font-bold", classNames?.text)}
-                onClick={onClick}
-            >
-                {text}
+            <span className={clsx("cursor-pointer", classNames?.text)} onClick={onClick}>
+                <span className="border-b border-primary font-bold">{text}</span>
+                {showArrow ? <DownArrowIcon size="medium" /> : ""}
             </span>
+
             <Popover.Content>
                 <div className={clsx("p-2", classNames?.children)}>{children}</div>
             </Popover.Content>
