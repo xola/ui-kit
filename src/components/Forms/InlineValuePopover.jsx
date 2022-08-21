96c2b9201081675f99fc25cda187d02f5c9d60f0
Rushi Vishavadia <rushi@xola.com>
2022-08-21 12:01:50 +0530

    Fix typo

--- src/components/Forms/InlineValuePopover.jsx
+++ src/components/Forms/InlineValuePopover.jsx
@@ -22,7 +22,7 @@ export const InlineValuePopover = ({
     };
 
     return (
-        <Popover visible={isOpen} sonClickOutside={onClickOutside} {...rest}>
+        <Popover visible={isOpen} onClickOutside={onClickOutside} {...rest}>
             <span className={clsx("cursor-pointer", classNames?.text)} onClick={handleClick}>
                 <span className="border-b border-b-black font-bold text-black">{text}</span>
                 {showArrow ? <DownArrowIcon size="medium" /> : ""}
