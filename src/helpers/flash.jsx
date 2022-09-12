7e8b0f3fe7b73f53109b689be766d0f4916c3c47
Rushi Vishavadia <rushi@xola.com>
2022-09-12 20:20:03 +0530

    Upgrade flash after talking to Anush

--- src/helpers/flash.jsx
+++ src/helpers/flash.jsx
@@ -5,23 +5,23 @@ import toast from "react-hot-toast";
 import { CloseIcon } from "../icons/CloseIcon";
 
 const colors = {
-    primary: "bg-primary-lighter border border-primary-light/20",
-    secondary: "bg-secondary-lighter border border-secondary-light",
-    success: "bg-success-lighter border border-success-light/20",
-    warning: "bg-warning-light border border-warning/30",
-    danger: "bg-danger-light border border-danger/30",
-    caution: "bg-caution-lighter border border-caution-light/20",
+    primary: "bg-primary",
+    secondary: "bg-secondary",
+    success: "bg-success",
+    warning: "bg-warning",
+    danger: "bg-danger",
+    caution: "bg-caution",
 };
 
 const sizes = {
     small: "px-3 py-2 text-sm leading-3.5 max-w-50",
-    medium: "px-4 py-3.5 text-base leading-4 max-w-100",
+    medium: "px-4 py-3.5 text-base leading-4 shadow max-w-100",
     large: "px-4.5 py-4 text-md leading-4.5 max-w-xl",
 };
 
 const defaultProps = {
     duration: 3000,
-    position: "bottom-right",
+    position: "top-center",
     reverseOrder: false,
 };
 
@@ -67,7 +67,7 @@ export const flash = {
     },
 
     getStyles(color, size, className) {
-        return clsx("flex text-black rounded pointer-events-auto", colors[color], sizes[size], className);
+        return clsx("flex text-white rounded pointer-events-auto", colors[color], sizes[size], className);
     },
 
     container(text, className, onClose, toastObject) {
@@ -79,22 +79,22 @@ export const flash = {
                 as={Fragment}
                 show={toastObject.visible}
                 enter="transition transform duration-500 ease-out"
-                enterFrom="translate-y-full !opacity-0"
-                enterTo="translate-y-0 !opacity-90"
+                enterFrom="-translate-y-full !opacity-0"
+                enterTo="translate-y-0 !opacity-100"
                 leave="transition transform duration-500 ease-in"
-                leaveFrom="!opacity-90"
-                leaveTo="!opacity-0"
+                leaveFrom="!opacity-90 translate-y-0"
+                leaveTo="!opacity-0 -translate-y-full"
             >
                 <div key={toastObject.id} className={className}>
                     <div className="w-full">{text}</div>
-                    {onClose ? (
+                    {onClose && (
                         <div
-                            className="flex cursor-pointer items-center justify-center pl-3 text-gray-dark hover:text-black"
+                            className="flex cursor-pointer items-center justify-center pl-3 opacity-60 hover:opacity-100"
                             onClick={onCloseWrapper}
                         >
                             <CloseIcon />
                         </div>
-                    ) : null}
+                    )}
                 </div>
             </Transition>
         );
