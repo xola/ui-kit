3a7cd42cfe6664ef3bf2ccff15c71aff6f1c7c7d
Alexander Khomyakov <akhomyakov@gmail.com>
2022-09-17 00:59:57 +0300

    Fix for a glitch on horizontal resize

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -16,7 +16,7 @@ export const Tabs = ({ className, variant = "default", value, onChange, children
 
     return (
         <>
-            <nav className={clsx("ui-tabs", "flex overflow-x-auto", variants[variant], className)} {...rest}>
+            <nav className={clsx("ui-tabs", "flex", variants[variant], className)} {...rest}>
                 {variant === "default" ? (
                     <div className="min-w-[40px] flex-shrink-0 border-b border-gray-light" />
                 ) : null}
