9fe5d836b487abb9979c41c0967ba156fbcad21d
Tanya <tanya@xola.com>
2022-10-19 12:09:45 +0530

    X2-3511: Add class to datepicker input

--- src/components/DatePicker/DatePickerPopover.jsx
+++ src/components/DatePicker/DatePickerPopover.jsx
@@ -64,6 +64,7 @@ export const DatePickerPopover = ({
                     readOnly
                     size="medium"
                     value={value ? formatDate(value, dateFormat) : ""}
+                    className={clsx(classNames.input ?? null)}
                     onClick={toggleVisibility}
                 />
             )}
