ce93c938ef9aacc904a0bddd065fd07879524847
Tanya Ranjan <tanyaranjan@Tanyas-MacBook-Pro.local>
2022-10-31 16:01:31 +0530

    X2-3511: Address review comment

--- src/components/DatePicker/DatePickerPopover.jsx
+++ src/components/DatePicker/DatePickerPopover.jsx
@@ -64,7 +64,7 @@ export const DatePickerPopover = ({
                     readOnly
                     size="medium"
                     value={value ? formatDate(value, dateFormat) : ""}
-                    className={classNames?.input ?? ""}
+                    className={classNames?.input}
                     onClick={toggleVisibility}
                 />
             )}
