12436017498926996468b9cf12403c62a5c2ec86
Zhyvotenko Oleksandr <oleksandr.zhyvotenko@mint.ai>
2023-04-17 16:32:11 +0300

    fix: fixed display required dot for currency input

--- src/components/Forms/BaseInput.jsx
+++ src/components/Forms/BaseInput.jsx
@@ -11,9 +11,10 @@ const sizes = {
 };
 
 export const BaseInput = ({ as: Tag, size = "medium", isError, className, isRequired, value, ...rest }) => {
+    const stringValue = isString(value) && value.replace("$", "").trim();
     // Added trim method for string value to prevent leading and trailing spaces.
     // Since the input can only be a string or a number, added the toString method for a numeric value, because lodash's IsEmpty method returns true for any number.
-    const isEmptyValue = isString(value) ? isEmpty(value?.trim()) : isEmpty(value?.toString());
+    const isEmptyValue = isString(value) ? isEmpty(stringValue) : isEmpty(value?.toString());
 
     return (
         <div className="relative flex w-full items-center">
