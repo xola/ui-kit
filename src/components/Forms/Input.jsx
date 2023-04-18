26783563c7010a5f3034e245cb8826e146848d63
SemenStruchev <semen.struchev@devico.io>
2023-04-18 14:31:34 +0300

    fix zero as first character in number input

--- src/components/Forms/Input.jsx
+++ src/components/Forms/Input.jsx
@@ -4,7 +4,21 @@ import React from "react";
 import { BaseInput } from "./BaseInput";
 
 export const Input = ({ className, type = "text", ...rest }) => {
-    return <BaseInput as="input" className={clsx("ui-input", className)} type={type} {...rest} />;
+    const preventFirstZero = (event) => {
+        if (event.target.value.charAt(0) === "0" && type === "number") {
+            event.target.value = event.target.value.slice(1);
+        }
+    };
+
+    return (
+        <BaseInput
+            as="input"
+            className={clsx("ui-input", className)}
+            type={type}
+            onKeyPress={(e) => preventFirstZero(e)}
+            {...rest}
+        />
+    );
 };
 
 Input.propTypes = {
