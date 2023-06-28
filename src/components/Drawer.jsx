ebed88b2e59fa3420b9920fae3e90c3d14303ca8
sachin <sachinp9797@gmail.com>
2023-06-28 11:40:25 +0530

    X2-6098: Pull up roster from Purchase item

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -42,7 +42,7 @@ export const Drawer = ({ isOpen = false, title, content, onClose, classNames = {
                             leaveFrom="translate-x-0"
                             leaveTo={position === "right" ? "translate-x-full" : "-translate-x-full"}
                         >
-                            <div className="flex w-screen max-w-xl md:max-w-screen-md">
+                            <div className="flex w-screen sm:max-w-screen-sm md:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl">
                                 {position === "right" ? <CloseButton onClose={onClose} /> : null}
 
                                 <div
