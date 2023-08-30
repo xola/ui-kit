2b814edbbb6e76e962108f11cb90a0f1398c83c2
Rushi Vishavadia <rushi@xola.com>
2023-08-30 13:17:43 +0530

    Fix lint in icon

--- src/icons/DumbBellIcon.jsx
+++ src/icons/DumbBellIcon.jsx
@@ -10,28 +10,28 @@ export const DumbbellIcon = createIcon((props) => {
                 strokeLinejoin="round"
                 strokeWidth="0.8"
                 d="M9.745 2.203a.687.687 0 000 .972l2.004 2.003a.687.687 0 10.972-.972l-2.004-2.003a.688.688 0 00-.972 0z"
-            ></path>
+            />
             <path
                 stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="0.8"
                 d="M7.801 2.202a.687.687 0 000 .973l3.949 3.948a.687.687 0 10.972-.974L8.775 2.202a.688.688 0 00-.974 0zM2.812 9.137a.688.688 0 01.972 0l2.004 2.003a.688.688 0 01-.973.972L2.812 10.11a.687.687 0 010-.972z"
-            ></path>
+            />
             <path
                 stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="0.8"
                 d="M2.812 7.192a.688.688 0 01.972 0l3.948 3.948a.688.688 0 11-.973.973L2.812 8.166a.688.688 0 010-.974z"
-            ></path>
+            />
             <path
                 stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="0.8"
                 d="M9.289 4.663L5.27 8.68l.973.972 4.017-4.017-.972-.972z"
-            ></path>
+            />
         </svg>
     );
 });
--- src/icons/MobileIcon.jsx
+++ src/icons/MobileIcon.jsx
@@ -11,12 +11,12 @@ export const MobileIcon = createIcon((props) => {
                 strokeWidth="1.3"
                 clipPath="url(#clip0_9896_373783)"
             >
-                <path d="M13.5 14.25a1.5 1.5 0 01-1.5 1.5H6a1.5 1.5 0 01-1.5-1.5V3.75A1.5 1.5 0 016 2.25h6a1.5 1.5 0 011.5 1.5v10.5z"></path>
-                <path d="M10.877 2.25v1.5a.375.375 0 01-.375.375h-3a.375.375 0 01-.375-.375v-1.5"></path>
+                <path d="M13.5 14.25a1.5 1.5 0 01-1.5 1.5H6a1.5 1.5 0 01-1.5-1.5V3.75A1.5 1.5 0 016 2.25h6a1.5 1.5 0 011.5 1.5v10.5z" />
+                <path d="M10.877 2.25v1.5a.375.375 0 01-.375.375h-3a.375.375 0 01-.375-.375v-1.5" />
             </g>
             <defs>
                 <clipPath id="clip0_9896_373783">
-                    <path fill="#fff" d="M0 0H18V18H0z"></path>
+                    <path fill="#fff" d="M0 0H18V18H0z" />
                 </clipPath>
             </defs>
         </svg>
