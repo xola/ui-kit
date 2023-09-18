29b30899245740a2be795c8839a7d3e2896f6cee
Meric Sentunali <merics@wirespring.com>
2023-09-18 15:32:18 +0300

    fix typo

--- src/icons/LightBulbIcon.jsx
+++ src/icons/LightBulbIcon.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createIcon } from "../helpers/icon";
 
-export const LigthBulbIcon = createIcon(() => {
+export const LightBulbIcon = createIcon(() => {
     return (
         <svg viewBox="0 0 16 16" width={16} height={16} fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M10 10.1278H6" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
@@ -22,4 +22,4 @@ export const LigthBulbIcon = createIcon(() => {
     );
 });
 
-LigthBulbIcon.tags = ["ligh", "bulb", "idea"];
+LightBulbIcon.tags = ["ligh", "bulb", "idea"];
