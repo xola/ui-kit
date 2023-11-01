0a6d9ff8991755df5d3c0abb2f874dbc3afe34c8
Rushi Vishavadia <rushi@xola.com>
2023-11-01 19:58:32 +0530

    Restore ES Lint

--- src/helpers/classnames.ts
+++ src/helpers/classnames.ts
@@ -1,6 +1,5 @@
 import { twMerge } from "tailwind-merge";
-import clsx from "clsx";
-import { ClassValue } from "clsx";
+import clsx, { ClassValue } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
