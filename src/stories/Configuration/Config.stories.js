29971af6e41059f5b38b7c74b10b1f2c01c0ce22
Rushi Vishavadia <rushi@xola.com>
2023-11-01 09:34:27 +0530

    Temp comment out react-json-view since it has issues with React 18

--- src/stories/Configuration/Config.stories.js
+++ src/stories/Configuration/Config.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactJson from "react-json-view";
+// import ReactJson from "react-json-view";
 import twConfig from "../../../tailwind.config";
 
 const ConfigStories = {
@@ -13,15 +13,15 @@ export const UIKitRawConfig = () => {
             <div className="mb-4">
                 This is the prettified version of <span className="font-mono">tailwind.config.js</span> file
             </div>
-            <ReactJson
-                src={theme}
-                name={false}
-                quotesOnKeys={false}
-                collapsed={2}
-                enableClipboard={false}
-                displayDataTypes={false}
-                style={{ fontSize: "12px", fontFamily: theme.extend.fontFamily.mono }}
-            />
+            {/*<ReactJson*/}
+            {/*    src={theme}*/}
+            {/*    name={false}*/}
+            {/*    quotesOnKeys={false}*/}
+            {/*    collapsed={2}*/}
+            {/*    enableClipboard={false}*/}
+            {/*    displayDataTypes={false}*/}
+            {/*    style={{ fontSize: "12px", fontFamily: theme.extend.fontFamily.mono }}*/}
+            {/*/>*/}
         </div>
     );
 };
