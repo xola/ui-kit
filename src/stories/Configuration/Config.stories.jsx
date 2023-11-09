307b7cf8cdaeb46763af3fc97bd0657b52d0995b
Rushi Vishavadia <rushi@xola.com>
2023-11-09 19:01:14 +0530

    Comment out JsonView because of JS/JSX/TSX

--- src/stories/Configuration/Config.stories.jsx
+++ src/stories/Configuration/Config.stories.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import JsonView from "react18-json-view";
-import "react18-json-view/src/style.css";
+// import JsonView from "react18-json-view";
+// import "react18-json-view/src/style.css";
 import twConfig from "../../../tailwind.config";
 
 const ConfigStories = {
@@ -14,12 +14,13 @@ export const UIKitRawConfig = () => {
             <div className="mb-4">
                 This is the prettified version of <span className="font-mono">tailwind.config.js</span> file
             </div>
-            <JsonView
-                src={theme}
-                collapsed={2}
-                enableClipboard={false}
-                style={{ fontSize: "12px", fontFamily: theme.extend.fontFamily.mono }}
-            />
+            <pre>{JSON.stringify(theme, null, 2)}</pre>
+            {/*<JsonView*/}
+            {/*    src={theme}*/}
+            {/*    collapsed={2}*/}
+            {/*    enableClipboard={false}*/}
+            {/*    style={{ fontSize: "12px", fontFamily: theme.extend.fontFamily.mono }}*/}
+            {/*/>*/}
         </div>
     );
 };
