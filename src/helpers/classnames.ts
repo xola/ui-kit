ea5de23371ebd2250c9bf9b2b13be1b1f5970638
Rushi Vishavadia <rushi@xola.com>
2023-11-13 20:03:29 +0530

    Add docs

--- src/helpers/classnames.ts
+++ src/helpers/classnames.ts
@@ -1,6 +1,10 @@
 import { twMerge } from "tailwind-merge";
 import clsx, { ClassValue } from "clsx";
 
+/**
+ * Utility function that uses `tailwind-merge` to efficiently merge Tailwind classes.
+ * See docs: https://github.com/dcastil/tailwind-merge/blob/v2.0.0/docs/what-is-it-for.md
+ */
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
