e473e5fabe7a237652dc5e514f11595ef7f6093c
Siddharth Singh <siddharth@xola.com>
2023-12-19 16:07:32 +0530

    Stories for accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,29 +1,28 @@
 import { Disclosure } from "@headlessui/react";
 import clsx from "clsx";
 import React, { Children } from "react";
+import PropTypes from "prop-types";
 import { SlideDown } from "../Animation/SlideDown";
 import { ChevronRightIcon } from "../../icons/ChevronRightIcon";
 
 export const Accordion = ({ children }) => {
     const childrenArray = Children.toArray(children);
     return (
-        <div>
-            <Disclosure>
-                {({ open }) => (
-                    <>
-                        <Disclosure.Button className="flex items-center">
-                            <div className="mr-2" data-testid="accordion-dropdown-icon">
-                                <ChevronRightIcon className={clsx(open && "rotate-90")} />
-                            </div>
+        <Disclosure>
+            {({ open }) => (
+                <>
+                    <Disclosure.Button className="flex items-center">
+                        <div className="mr-2" data-testid="accordion-dropdown-icon">
+                            <ChevronRightIcon className={clsx(open && "rotate-90")} />
+                        </div>
 
-                            {childrenArray.filter((child) => child?.type === Header)}
-                        </Disclosure.Button>
+                        {childrenArray.filter((child) => child?.type === Header)}
+                    </Disclosure.Button>
 
-                        <SlideDown isOpen={open}>{childrenArray.filter((child) => child?.type === Body)}</SlideDown>
-                    </>
-                )}
-            </Disclosure>
-        </div>
+                    <SlideDown isOpen={open}>{childrenArray.filter((child) => child?.type === Body)}</SlideDown>
+                </>
+            )}
+        </Disclosure>
     );
 };
 
@@ -46,3 +45,17 @@ const Body = ({ className, children, ...rest }) => {
 };
 
 Accordion.Body = Body;
+
+Accordion.prototype = {
+    children: PropTypes.node,
+};
+
+Header.prototype = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+};
+
+Body.prototype = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+};
