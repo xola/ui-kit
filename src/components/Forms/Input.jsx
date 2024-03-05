29ad316a8a6275db2d6791ed04777bdf1d87919e
kirtesh-xola <kirtesh@xola.com>
2024-03-05 16:17:12 +0530

    fix(input): adds support for `prefix` and `suffix` while showing isRequired indicator

--- src/components/Forms/Input.jsx
+++ src/components/Forms/Input.jsx
@@ -1,11 +1,13 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { forwardRef } from "react";
 import { BaseInput } from "./BaseInput";
 
-export const Input = ({ className, type = "text", value, ...rest }) => {
-    return <BaseInput as="input" className={clsx("ui-input", className)} type={type} value={value} {...rest} />;
-};
+export const Input = forwardRef(({ className, type = "text", value, ...rest }, ref) => {
+    return (
+        <BaseInput ref={ref} as="input" className={clsx("ui-input", className)} type={type} value={value} {...rest} />
+    );
+});
 
 Input.propTypes = {
     ...BaseInput.propTypes,
