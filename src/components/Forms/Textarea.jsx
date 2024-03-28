474ef2abc4b859ef10b723417c37bd609b6e97fa
Akash Babber <akash.babber@gmail.com>
2024-03-28 16:35:05 +0530

    XS-80: added auto size textarea (#318)
    * XS-80: added auto size textarea
    
    * XS-80: merge commit fixes
    
    * XS-80: lint fix
    
    * XS-80: fixed story
    
    * Format + Add one-line documentation
    
    * XS-80: fixed rows prop for textarea
    
    * XS-80: prop order change
    
    ---------
    
    Co-authored-by: Rushi Vishavadia <rushi@xola.com>

--- src/components/Forms/Textarea.jsx
+++ src/components/Forms/Textarea.jsx
@@ -1,16 +1,27 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import React, { forwardRef } from "react";
+import TextareaAutosize from "react-textarea-autosize";
 import { BaseInput } from "./BaseInput";
 
-export const Textarea = forwardRef(({ className, value, rows = 2, ...rest }, ref) => {
-    return (
+export const Textarea = forwardRef(({ className, value, shouldAutoSize = false, rows = 2, ...rest }, ref) => {
+    return shouldAutoSize ? (
+        // TextareaAutosize has a few special props like minRows and maxRows instead of rows field so using rows field to feed the minRows prop. See full list here https://github.com/Andarist/react-textarea-autosize?tab=readme-ov-file#props
+        <BaseInput
+            ref={ref}
+            as={TextareaAutosize}
+            value={value}
+            minRows={rows}
+            className={clsx("ui-textarea", className)}
+            {...rest}
+        />
+    ) : (
         <BaseInput
             ref={ref}
             as="textarea"
             value={value}
-            className={clsx("ui-textarea", className)}
             rows={rows}
+            className={clsx("ui-textarea", className)}
             {...rest}
         />
     );
@@ -19,8 +30,10 @@ export const Textarea = forwardRef(({ className, value, rows = 2, ...rest }, ref
 Textarea.propTypes = {
     ...BaseInput.propTypes,
     rows: PropTypes.number,
+    shouldAutoSize: PropTypes.bool,
 };
 
 Textarea.defaultProps = {
     rows: 2,
+    shouldAutoSize: false,
 };
