2294edbcfaa69c489b78fe4bb3ac6bebedf620bb
VitalyDevico <vitaly@devico.io>
2024-06-05 15:37:20 +0300

    lint

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -92,7 +92,7 @@ const CloseButton = ({ onClose }) => {
             size="small"
             variant="link"
             className={clsx(
-                "!text-black m-2.5 inline-flex !h-10 !w-10 items-center justify-center !rounded-full bg-white !px-1.5",
+                "m-2.5 inline-flex !h-10 !w-10 items-center justify-center !rounded-full bg-white !px-1.5 !text-black",
             )}
             onClick={onClose}
         >
