88d1b87f5e0ca6acb490dfd66cd1f0bacef09638
Oleksandr Khomyakov <10237454+khomyakov@users.noreply.github.com>
2024-07-04 23:40:03 +0300

    Reverting some leftover changes

--- src/components/Forms/Input.jsx
+++ src/components/Forms/Input.jsx
@@ -1,20 +1,13 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import React, { forwardRef } from "react";
+import React from "react";
 import { BaseInput } from "./BaseInput";
 
-export const Input = forwardRef(({ className, type = "text", value, ...rest }, ref) => {
-    return (
-        <BaseInput ref={ref} as="input" className={clsx("ui-input", className)} type={type} value={value} {...rest} />
-    );
-});
+export const Input = ({ className, type = "text", value, ...rest }) => {
+    return <BaseInput as="input" className={clsx("ui-input", className)} type={type} value={value} {...rest} />;
+};
 
 Input.propTypes = {
     ...BaseInput.propTypes,
     type: PropTypes.string,
-};
-
-Input.defaultProps = {
-    ...BaseInput.defaultProps,
-    type: "text",
 };
--- src/components/Forms/Textarea.jsx
+++ src/components/Forms/Textarea.jsx
@@ -1,26 +1,13 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import React, { forwardRef } from "react";
+import React from "react";
 import { BaseInput } from "./BaseInput";
 
-export const Textarea = forwardRef(({ className, value, rows = 2, ...rest }, ref) => {
-    return (
-        <BaseInput
-            ref={ref}
-            as="textarea"
-            value={value}
-            className={clsx("ui-textarea", className)}
-            rows={rows}
-            {...rest}
-        />
-    );
-});
+export const Textarea = ({ className, rows = 2, ...rest }) => {
+    return <BaseInput as="textarea" className={clsx("ui-textarea", className)} rows={rows} {...rest} />;
+};
 
 Textarea.propTypes = {
     ...BaseInput.propTypes,
     rows: PropTypes.number,
-};
-
-Textarea.defaultProps = {
-    rows: 2,
 };
