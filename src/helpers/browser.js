28fe010eb9fe39b7f6a6e844e6f7c9dde4531d00
Oleksandr Khomyakov <10237454+khomyakov@users.noreply.github.com>
2024-07-04 23:24:00 +0300

    Ios browser detection

--- src/helpers/browser.js
+++ src/helpers/browser.js
@@ -1,2 +1,16 @@
 // eslint-disable-next-line no-undef
 export const isOSX = typeof window === "undefined" ? false : window.navigator.userAgent.includes("Macintosh");
+
+export const isIosBrowser = () => {
+    if (typeof window === 'undefined' || !window.navigator) return false;
+  
+    const ua = window.navigator.userAgent;
+    const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+    const webkit = !!ua.match(/WebKit/i);
+  
+    // Additional check for iPad with iOS 13+
+    const iPad = !!ua.match(/Macintosh/i) && navigator.maxTouchPoints > 1;
+  
+    return (iOS && webkit) || iPad;
+  };
+  
