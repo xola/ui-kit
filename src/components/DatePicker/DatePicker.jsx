980ce4321908377dacd0a5111d9bfcc332962a4c
shivbang <shivani.bang98@gmail.com>
2024-08-19 12:24:54 +0530

    X2-10394 fixing undefined getTime

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -166,6 +166,12 @@ export const DatePicker = ({
                     options,
                     event,
                 );
+            } else {
+                onChange(
+                    DateUtils.addDayToRange(toDate(now(day, timezoneName).endOf("day"), false), value),
+                    options,
+                    event,
+                );
             }
         } else {
             onChange(toDate(now(day, timezoneName)), options, event);
