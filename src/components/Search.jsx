62dd7cf4a796105c5fcdbf4450a92aa14177c807
Ajay Makkapati <ajay@xola.com>
2024-08-27 14:14:04 +0530

    X2-10410 Dashboard search overlap issue fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -175,7 +175,7 @@ export const Search = ({
                     id: menuId,
                     className: clsx(
                         "ui-search-menu",
-                        "absolute top-10 divide-y divide-gray-light w-full xl:w-[590px] max-h-[75vh] border border-secondary-lighter shadow-xl mt-1 rounded overflow-auto z-10 bg-white",
+                        "absolute top-10 divide-y divide-gray-light w-full xl:w-[590px] max-h-[75vh] border border-secondary-lighter shadow-xl mt-1 rounded overflow-auto z-20 bg-white",
                         { hidden: !open },
                     ),
                 })}
