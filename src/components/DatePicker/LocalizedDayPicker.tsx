ff00cfea607887ce5df95bcc3691f9962621c0e4
Manoj Vaibhav <manoj.r@xola.com>
2024-09-18 12:00:00 +0530

    XS-414 weekdays bug fix and removed unnecessary console log (#348)
    * 2.3.13
    
    * XS-414 Fixed issue

--- src/components/DatePicker/LocalizedDayPicker.tsx
+++ src/components/DatePicker/LocalizedDayPicker.tsx
@@ -1,22 +1,26 @@
 import clsx from "clsx";
 import React, { forwardRef, useContext, useEffect, useState } from "react";
 import DayPicker, { DayPickerProps } from "react-day-picker";
+import { kebabCase } from "lodash";
 import { Context } from "../Provider";
 import { getLocalizationProps, LocaleCode, LocalizationProps } from "./DatePicker.helpers";
 
 export const LocalizedDayPicker = forwardRef<any, DayPickerProps>(({ className, ...rest }, ref) => {
     const { locale } = useContext(Context);
     const [localizationProps, setLocalizationProps] = useState<Partial<LocalizationProps>>({});
-    console.log("Locale", locale);
 
     useEffect(() => {
         setLocalizationProps({});
+        const localeCode = kebabCase(locale).toLowerCase();
 
         /** We don't want any localization-related props for "English" */
-        if (!locale || locale === "en" || locale === "en_US") return;
+        if (!localeCode || localeCode === "en" || localeCode === "en-us") return;
 
         getLocalizationProps(locale as LocaleCode).then(setLocalizationProps);
     }, [locale]);
 
+    /**
+     * Note: DayPicker expects locale in en-US, es, en-GB format
+     */
     return <DayPicker ref={ref} className={clsx(className)} {...localizationProps} {...rest} />;
 });
