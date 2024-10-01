fae045336cc7004d88c248a8f0aec4dfabe98587
SemenStruchev <125129104+SemenStruchev@users.noreply.github.com>
2024-10-01 13:58:17 +0300

    X2-9688 added loading state for Arrival picker dates (#349)
    * added loading state for Arrival picker dates
    
    * fix error
    
    * resolve feedback
    
    * fix crush
    
    * fix lint
    
    * fix loading status
    
    * resolve feedback

--- src/components/Dot/Dot.jsx
+++ src/components/Dot/Dot.jsx
@@ -15,6 +15,7 @@ const sizes = {
     small: "h-1 w-1",
     medium: "h-1.5 w-1.5",
     large: "h-2 w-2",
+    xlarge: "h-8 w-8",
 };
 
 export const Dot = ({ color = "primary", size = "medium", className, ...rest }) => {
