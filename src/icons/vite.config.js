7e73d7d4d28735a3c98afe53da1fecae17341564
Rushi Vishavadia <rushi@xola.com>
2024-12-21 12:10:37 +0530

    Update icons package, package.json cleanup and version

---
+++ src/icons/vite.config.js
@@ -0,0 +1,23 @@
+import path from "path";
+import { defineConfig } from "vite";
+import pkg from "./package.json";
+
+const dependencies = Object.keys(pkg.dependencies);
+
+export default defineConfig({
+    build: {
+        outDir: "build",
+
+        lib: {
+            entry: path.resolve(__dirname, "./index.js"),
+            name: "XolaIcons",
+            fileName: (format) => `icons.${format}.js`,
+            formats: ["es"]
+        },
+
+        rollupOptions: {
+            // Make sure none of the dependencies are bundled.
+            external: dependencies,
+        }
+    }
+});
