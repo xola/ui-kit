897a6e54731da7c056063681e325be9f2f2c1870
Rushi Vishavadia <rushi@xola.com>
2024-12-21 18:56:24 +0530

    Remove tsup and ts, back to vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import pkg from "./package.json";
+import copy from "rollup-plugin-copy";
 
 const dependencies = Object.keys(pkg.dependencies);
 const devDependencies = Object.keys(pkg.devDependencies);
@@ -13,13 +14,24 @@ export default defineConfig({
         lib: {
             entry: path.resolve(__dirname, "src/index.js"),
             name: "XolaUIKit",
-            fileName: (format) => `ui-kit.${format}.js`,
-            formats: ["es"]
+            // fileName: (format) => `${pkg.name}.${format}.js`,
+            formats: ["es", "cjs"],
         },
 
         rollupOptions: {
             // Make sure none of the dependencies are bundled.
             external: [...dependencies, ...devDependencies],
+            plugins: [
+                copy({
+                    hook: "writeBundle",
+                    targets: [
+                        { src: "index.css", dest: "build" },
+                        { src: "index.d.ts", dest: "build" },
+                        { src: "tailwind.config.cjs", dest: "build" },
+                        { src: "postcss.config.cjs", dest: "build" },
+                    ],
+                }),
+            ],
             // Leave commented out - testing multiple outputs
             // input: {
             //     server: "src/utils/index.js",
