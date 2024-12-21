c8f8c2cc304c77ed05a474f54397cf60d12b6e94
Rushi Vishavadia <rushi@xola.com>
2024-12-21 12:10:37 +0530

    Remove prop-types from icons package

--- src/icons/src/helpers/icon.jsx
+++ src/icons/src/helpers/icon.jsx
@@ -1,16 +1,10 @@
 import cn from "./classnames";
-import PropTypes from "prop-types";
 import React from "react";
 import { iconSizes } from "./iconSizes";
 
 export const createIcon = (Icon) => {
     const IconContainer = ({ size = "small", className, ...rest }) => {
         return <Icon className={cn(iconSizes[size], "relative -top-0.25 inline-block", className)} {...rest} />;
-    };
-
-    IconContainer.propTypes = {
-        size: PropTypes.oneOf(Object.keys(iconSizes)),
-        className: PropTypes.string,
     };
 
     return IconContainer;
