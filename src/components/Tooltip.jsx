c96e6a073223839ee52ac8b7e917cfcb406e2ed7
Rushi Vishavadia <rushi@xola.com>
2024-12-23 12:09:13 +0530

    Add tippy animation css into UI Kit itself

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -3,6 +3,11 @@ import PropTypes from "prop-types";
 import React from "react";
 import { followCursor } from "tippy.js"; // Dont remove this even if unused. It is required for one prop
 import "tippy.js/dist/tippy.css"; // If we customize the style, the change this and import our own style
+// Do not remove these https://atomiks.github.io/tippyjs/v6/animations/
+import "tippy.js/animations/perspective.css";
+import "tippy.js/animations/scale.css";
+import "tippy.js/animations/shift-away.css";
+import "tippy.js/animations/shift-toward.css";
 
 // TODO: Implement "as='div'"
 export const Tooltip = ({ children, className, content, ...rest }) => {
