3c8d74d54f7a6b4b9222cdf124e5a3f7708f25a7
Karthick <karthick@xola.com>
2025-05-15 17:35:02 +0530

    WIP - Month picker

--- src/components/DatePicker/MonthSelector.jsx
+++ src/components/DatePicker/MonthSelector.jsx
@@ -3,33 +3,38 @@ import PropTypes from "prop-types";
 import React, { useState } from "react";
 import clsx from "clsx";
 import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from "../../icons";
+import { Button } from "../Buttons/Button";
 
 const today = dayjs();
 
 export const MonthSelector = ({ date, locale, onChange, currentMonth }) => {
     const months = [...Array.from({ length: 12 }).keys()].map((m) => today.locale(locale).month(m).format("MMM"));
+    const [isSelectingMonth, setIsSelectingMonth] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(date);
+    console.log("date", date);
+    console.log("currentMonth", currentMonth);
+    console.log("selectedDate", selectedDate);
 
     const handleMonthSelect = (monthIndex) => {
-        const newDate = new Date(currentMonth.getFullYear(), monthIndex);
+        const newDate = new Date(selectedDate.getFullYear(), monthIndex);
         console.log("handleMonthSelect", newDate);
         onChange(newDate);
         setIsSelectingMonth(false);
     };
 
     const handleYearChange = (offset) => {
-        const newDate = new Date(currentMonth);
+        const newDate = new Date(selectedDate);
         newDate.setFullYear(newDate.getFullYear() + offset);
         console.log("handleYearChange", newDate);
-        onChange(newDate);
+        setSelectedDate(newDate);
+        // onChange(newDate);
     };
-
-    const [isSelectingMonth, setIsSelectingMonth] = useState(false);
 
     const handleClear = () => {
         // setSelectedDate(undefined);
         // onChange(undefined);
         onChange(new Date());
-        setIsSelectingMonth(false);
+        // setIsSelectingMonth(false);
     };
 
     const handleToday = () => {
@@ -38,17 +43,20 @@ export const MonthSelector = ({ date, locale, onChange, currentMonth }) => {
         setIsSelectingMonth(false);
     };
 
+    const isCurrentYear = selectedDate.getFullYear() === currentMonth.getFullYear();
+    console.log("selectedYear is same as currentMonth year", isCurrentYear);
+
     return (
-        <span className="DayPicker-Caption space-x-2">
+        <span className="DayPicker-Caption">
             <span className="inline-block">
                 {isSelectingMonth ? (
-                    <span className="  absolute  z-10 rounded-lg border border-gray  bg-white p-2 shadow-md">
-                        <div className="mb-4 flex items-center justify-between">
+                    <span className="absolute top-0 z-10 rounded-lg border border-gray  bg-white p-3 shadow-md">
+                        <div className="mb-4 flex items-center justify-between ">
                             <ChevronButton isVisible onClick={() => handleYearChange(-1)}>
                                 <ChevronLeftIcon />
                             </ChevronButton>
 
-                            <span className="text-lg font-medium">{date.getFullYear()}</span>
+                            <span className="text-lg font-bold">{selectedDate.getFullYear()}</span>
                             <ChevronButton isVisible onClick={() => handleYearChange(1)}>
                                 <ChevronRightIcon />
                             </ChevronButton>
@@ -60,7 +68,9 @@ export const MonthSelector = ({ date, locale, onChange, currentMonth }) => {
                                     key={month}
                                     type="button"
                                     className={clsx(
-                                        date.getMonth() === index ? "bg-blue-dark text-white" : "text-black",
+                                        date.getMonth() === index && isCurrentYear
+                                            ? "bg-blue-dark text-white"
+                                            : "text-black",
                                         "rounded-md p-4 text-center hover:bg-blue-dark hover:text-white",
                                     )}
                                     onClick={() => handleMonthSelect(index)}
@@ -69,22 +79,13 @@ export const MonthSelector = ({ date, locale, onChange, currentMonth }) => {
                                 </button>
                             ))}
                         </div>
-
-                        <div className="border-gray-200 flex justify-between border-t p-4">
-                            <button
-                                type="button"
-                                className="border-gray-300 text-gray-700 hover:bg-gray-50 rounded border bg-white px-4 py-2 transition-colors"
-                                onClick={handleClear}
-                            >
+                        <div className="flex justify-between border-t border-gray-lighter p-2">
+                            <Button size="small" color="secondary" variant="outline" onClick={handleClear}>
                                 Clear
-                            </button>
-                            <button
-                                type="button"
-                                className="border-gray-300 text-gray-700 hover:bg-gray-50 rounded border bg-white px-4 py-2 transition-colors"
-                                onClick={handleToday}
-                            >
+                            </Button>
+                            <Button size="small" color="secondary" variant="outline" onClick={handleToday}>
                                 Today
-                            </button>
+                            </Button>
                         </div>
                     </span>
                 ) : (
@@ -92,7 +93,7 @@ export const MonthSelector = ({ date, locale, onChange, currentMonth }) => {
                         className="flex cursor-pointer items-center justify-between"
                         onClick={() => setIsSelectingMonth(true)}
                     >
-                        <span className="text-lg font-medium">
+                        <span className="pr-1 text-lg font-bold">
                             {date.toLocaleString("default", { month: "long", year: "numeric" })}
                         </span>
                         <ChevronDownIcon />
