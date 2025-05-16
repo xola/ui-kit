e4ff5675b66c45413e3d88b2f244ceb93956f3ac
Karthick <karthick@xola.com>
2025-05-16 20:08:17 +0530

    WIP - Month picker popover

--- src/components/DatePicker/MonthSelector.jsx
+++ src/components/DatePicker/MonthSelector.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 import React, { useState } from "react";
 import { ChevronDownIcon } from "../../icons";
-import { Button } from "../Buttons/Button";
 import { MonthGrid } from "./MonthGrid";
 
 export const MonthSelector = ({ date, locale, onChange }) => {
