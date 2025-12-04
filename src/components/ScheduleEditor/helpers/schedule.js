3c4ac5be02fe5bac043c433bb5f07623e23aa6fd
Manoj Vaibhav <manoj.r@xola.com>
2025-12-04 11:16:06 +0530

    X2-13325 | Added support for AllowedPrivacies in Schedule Editor (#378)
    * X2-13325 Introduced AllowedPrivacies field for Schedule Editor
    
    * X2-13325 Updatd package.json
    
    * X2-13325 Showing error message for allowedPrivacies
    
    * X2-13325 Showing error
    
    * X2-13325 Temp fix
    
    * X2-13325 refactor
    
    * X2-13325 Removed console.log

--- src/components/ScheduleEditor/helpers/schedule.js
+++ src/components/ScheduleEditor/helpers/schedule.js
@@ -10,5 +10,6 @@ export const getScheduleDefaultValues = () => {
         priceDeltaType: "",
         times: [],
         timeRanges: [],
+        allowedPrivacies: ["public", "private"],
     };
 };
