4042b077d7000c33b6cf31902e448882f1bdab17
Rushi Vishavadia <rushi@xola.com>
2025-12-07 11:34:21 +0530

    Remove jsx files that

--- src/components/Forms/Select.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { BaseInput } from "./BaseInput";
-
-export const Select = (props) => {
-    return <BaseInput as="select" {...props} />;
-};
-
-Select.propTypes = BaseInput.propTypes;
