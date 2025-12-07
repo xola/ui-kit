edc36528041e68ead9596b54503db4ae4e70097e
Rushi Vishavadia <rushi@xola.com>
2025-12-07 12:06:24 +0530

    Upgrade to using new version of @xola/jslint that provides rules only

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import xolaLint from "@xola/jslint";
+import progress from "eslint-plugin-progress";
+
+xolaLint[1].settings.progress = {
+    skipStats: false,
+    skipSlowFiles: true,
+    interval: 2500,
+};
+xolaLint[1].plugins.progress = progress;
+
+xolaLint[0].ignores.push(".next");
+export default [...xolaLint];
