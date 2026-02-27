358721a8d97353557990c4dfe19cfe90ab100933
KarthickXola <karthick@xola.com>
2026-02-27 17:27:35 +0530

    added icons

--- src/icons/src/ItemsIcon.jsx
+++ src/icons/src/ItemsIcon.jsx
@@ -4,7 +4,13 @@ import { createIcon } from "./helpers/icon";
 export const ItemsIcon = createIcon((props) => {
     return (
         <svg viewBox="0 0 24 24" width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-            <g clipPath="url(#clip0_14983_75633)" stroke="currentColor" strokeWidth="1.125" strokeLinecap="round" strokeLinejoin="round">
+            <g
+                clipPath="url(#clip0_14983_75633)"
+                stroke="currentColor"
+                strokeWidth="1.125"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
                 <path d="M5 16H7" />
                 <path d="M13 19H4C2.895 19 2 18.105 2 17V7.569C2 7.299 2.055 7.031 2.161 6.783L3.259 4.214C3.574 3.478 4.297 3 5.098 3H14.902C15.703 3 16.426 3.478 16.741 4.214L17.839 6.783C17.945 7.032 18 7.299 18 7.569V11.1" />
                 <path d="M10 3V7.44" />
