d701667b14b97f5570839a50bc22314c23bca5e1
Rushi Vishavadia <rushi@xola.com>
2026-07-12 21:24:34 +0530

    fix(storybook): replace deprecated addons with styling-webpack
    Replaced @storybook/addon-postcss and storybook-css-modules-preset with @storybook/addon-styling-webpack, the officially recommended Storybook 8 successor. Configured explicit webpack rules for CSS Modules and plain CSS. Reduces npm audit findings from 16 to 7, resolving the final non-vitest/vite vulnerability cluster.

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,13 +9,52 @@ module.exports = {
 
     addons: [
         "@storybook/addon-webpack5-compiler-babel",
-        "@storybook/addon-postcss",
         "@storybook/addon-links",
-        "storybook-css-modules-preset",
         {
             name: "@storybook/addon-essentials",
             options: {
                 backgrounds: false,
+            },
+        },
+        {
+            name: "@storybook/addon-styling-webpack",
+            options: {
+                rules: [
+                    {
+                        test: /\.module\.css$/,
+                        use: [
+                            "style-loader",
+                            {
+                                loader: "css-loader",
+                                options: {
+                                    importLoaders: 1,
+                                    modules: {
+                                        auto: true,
+                                        namedExport: false,
+                                        exportLocalsConvention: "camel-case-only",
+                                        localIdentName: "[name]__[local]--[hash:base64:5]",
+                                    },
+                                },
+                            },
+                            {
+                                loader: "postcss-loader",
+                                options: { implementation: require.resolve("postcss") },
+                            },
+                        ],
+                    },
+                    {
+                        test: /\.css$/,
+                        exclude: /\.module\.css$/,
+                        use: [
+                            "style-loader",
+                            { loader: "css-loader", options: { importLoaders: 1 } },
+                            {
+                                loader: "postcss-loader",
+                                options: { implementation: require.resolve("postcss") },
+                            },
+                        ],
+                    },
+                ],
             },
         },
     ],
