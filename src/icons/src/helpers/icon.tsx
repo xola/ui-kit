b94a6e311d8829adb49823e3125094c67e31a23f
Rushi Vishavadia <rushi@xola.com>
2026-08-28 15:26:00 +0530

    Enable tree-shaking for @xola/icons (#447)

--- src/icons/src/helpers/icon.tsx
+++ src/icons/src/helpers/icon.tsx
@@ -8,7 +8,7 @@ export interface IconProps extends React.SVGProps<SVGSVGElement> {
 
 export type IconComponent = React.FC<IconProps> & { tags?: string[] };
 
-export const createIcon = (Icon: (props: IconProps) => JSX.Element): IconComponent => {
+export const createIcon = (Icon: (props: IconProps) => React.ReactElement): IconComponent => {
     const IconContainer: IconComponent = ({ size = "small", className, ...rest }) => {
         return <Icon className={clsx(iconSizes[size], "relative -top-0.25 inline-block", className)} {...rest} />;
     };
