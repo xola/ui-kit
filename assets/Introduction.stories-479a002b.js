7831d4dd393936dd4c52466ebe9a28516c5ffc54
rushi <rushi@users.noreply.github.com>
2026-09-04 05:32:39 +0000

    Publish 'storybook-static' to 'gh-pages' in 'xola/ui-kit'

---
+++ assets/Introduction.stories-479a002b.js
@@ -0,0 +1,27 @@
+import{M as a}from"./index-848fea6a.js";import{j as o,a as n,F as s}from"./jsx-runtime-5e7b5774.js";import{u as l}from"./index-3170f51e.js";import"./iframe-20182469.js";import"../sb-preview/runtime.js";import"./chunk-6E673XPF-4294b5bd.js";import"./index-e6e5af86.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-33434671.js";import"./index-1ae9f0af.js";import"./index-1b441bc2.js";import"./index-47bbf582.js";import"./isSymbol-939a2475.js";import"./index-356e4a49.js";function i(t){const e=Object.assign({h2:"h2",p:"p",em:"em",h3:"h3",a:"a",ul:"ul",li:"li",code:"code"},l(),t.components);return n(s,{children:[o(a,{title:"Introduction"}),`
+`,o(e.h2,{id:"welcome-to-the-xola-ui-kit",children:"Welcome to the Xola UI Kit"}),`
+`,o(e.p,{children:"This UI Kit is a suite of React + Tailwind components & helper libraries that conform to Xola's design system and brand guidelines which are set by Xola's design team. Components like alerts, avatars, modals, tooltips etc."}),`
+`,o(e.p,{children:"The UI Kit ensures consistent & best practices. It has a friendly development experience by reducing the time you need to spend working on configuring the UX of a component. Everything you see here was built from scratch for the new Xola brand."}),`
+`,n(e.p,{children:["Click on each component on the left to view it's design. Click on ",o(e.em,{children:'"Docs"'})," in the top toolbar to look at it's configuration parameters which can be tried out live! Click ",o(e.em,{children:'"Show Code"'})," in those boxes to see how to use that component."]}),`
+`,o(e.h3,{id:"install-the-ui-kit",children:"Install the UI Kit"}),`
+`,n(e.p,{children:["See ",o(e.a,{href:"/story/installation--page",children:"Installation"})," for installations & usage instructions"]}),`
+`,o(e.h3,{id:"core-technologies",children:"Core Technologies"}),`
+`,n(e.ul,{children:[`
+`,n(e.li,{children:["React v17 (using ",o(e.a,{href:"https://reactjs.org/docs/hooks-overview.html",target:"_blank",rel:"nofollow noopener noreferrer",children:"hooks"}),")"]}),`
+`,n(e.li,{children:[o(e.a,{href:"https://tailwindcss.com",target:"_blank",rel:"nofollow noopener noreferrer",children:"Tailwind CSS"})," v3.x"]}),`
+`,n(e.li,{children:[o(e.a,{href:"https://storybook.js.org/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Storybook JS"})," v7.x"]}),`
+`,n(e.li,{children:[o(e.a,{href:"https://vitejs.dev/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Vite"})," - Frontend Tooling: Dev Server & JS Compilation"]}),`
+`]}),`
+`,n(e.p,{children:["See ",o(e.code,{children:"package.json"})," for other packages used"]}),`
+`,o(e.h3,{id:"links",children:"Links"}),`
+`,n(e.ul,{children:[`
+`,o(e.li,{children:"The Storybook at https://ui.xola.io"}),`
+`,n(e.li,{children:["The source code on Github ",o(e.a,{href:"https://github.com/xola/ui-kit",target:"_blank",rel:"nofollow noopener noreferrer",children:"github.com/xola/ui-kit"})]}),`
+`,n(e.li,{children:["The NPM package at ",o(e.a,{href:"https://www.npmjs.com/package/@xola/ui-kit",target:"_blank",rel:"nofollow noopener noreferrer",children:"@xola/ui-kit"})]}),`
+`]}),`
+`,o(e.h3,{id:"the-magicians",children:"The Magicians"}),`
+`,n(e.ul,{children:[`
+`,n(e.li,{children:["Design - ",o(e.a,{href:"https://www.callbruno.com/en/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Barthélémy Chalvet & Team Bruno"}),", J Scott Zimmerman & Anush Ramani"]}),`
+`,n(e.li,{children:["Coding - Nemanja Krstić, Rushi Vishavadia ",o(e.a,{href:"https://github.com/xola/ui-kit/graphs/contributors",target:"_blank",rel:"nofollow noopener noreferrer",children:"and more"})]}),`
+`]}),`
+`,o(e.p,{children:"⚡️ Welcome to the Xola v2 Magic 🪄"})]})}function c(t={}){const{wrapper:e}=Object.assign({},l(),t.components);return e?o(e,{...t,children:o(i,{...t})}):i(t)}const h=()=>{throw new Error("Docs-only story")};h.parameters={docsOnly:!0};const r={title:"Introduction",tags:["stories-mdx"],includeStories:["__page"]};r.parameters=r.parameters||{};r.parameters.docs={...r.parameters.docs||{},page:c};const C=["__page"];export{C as __namedExportsOrder,h as __page,r as default};
